d0d32036fd5b4ea92cf0e28fc6cf8b7d70fbf419
Daniel <Daniel.McInnes@qinetic.com.au>
2021-11-01 15:03:27 +1000

    Add developer switch for languages

---
+++ language.h
@@ -0,0 +1,43 @@
+/*
+** Copyright (C) 2021 Victron Energy B.V.
+*/
+
+#ifndef LANGUAGE_H
+#define LANGUAGE_H
+
+#include <QLocale>
+#include <QObject>
+#include <QString>
+
+class QQmlEngine;
+
+namespace Victron {
+
+namespace VenusOS {
+
+class Language : public QObject
+{
+	Q_OBJECT
+	Q_PROPERTY(QLocale::Language current READ getCurrentLanguage WRITE setCurrentLanguage NOTIFY currentLanguageChanged FINAL)
+public:
+	explicit Language(QQmlEngine* engine);
+
+	Q_ENUM(QLocale::Language)
+
+	Q_INVOKABLE QString toString(QLocale::Language language) const;
+	QLocale::Language getCurrentLanguage() const;
+	void setCurrentLanguage(QLocale::Language language);
+
+Q_SIGNALS:
+	void currentLanguageChanged();
+
+private:
+	QLocale::Language m_currentLanguage = QLocale::English;
+	QQmlEngine* m_qmlEngine = nullptr;
+};
+
+} /* VenusOS */
+
+} /* Victron */
+
+#endif // LANGUAGE_H
