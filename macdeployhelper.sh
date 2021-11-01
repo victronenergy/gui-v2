eca8c9a7705e0bb05e38c7ce38cb39fd9b2c178c
Chris Adams <chris.adams@qinetic.com.au>
2021-11-01 11:24:46 +1000

    Add deployment scripts for Windows and MacOSX
    Also tweak the build output paths slightly to simplify deployment.

---
+++ macdeployhelper.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+
+# macdeployhelper.sh - ease creation of deployment artifacts for MacOSX.
+
+# modify the following variables to match your system's build kit settings
+QT_LIBS_BIN_DIR=~/Qt/6.2.1/macos/bin
+
+# modify the following variable to point to the build output directory
+BUILD_OUT_DIR=../build-gui-v2-Qt_6_2_1_for_macOS-Release
+
+# don't modify anything below
+EXE_NAME=venus-gui-v2
+PATH=$QT_LIBS_BIN_DIR:$PATH
+APP_BUNDLE_DIR=$BUILD_OUT_DIR/bin/venus-gui-v2.app
+DEPLOYMENT_DIR=$APP_BUNDLE_DIR/Contents/MacOS/
+DHQML=$DEPLOYMENT_DIR/MacDeployHelper.qml
+DHQMLDIR=$DEPLOYMENT_DIR/qmldir
+
+echo import QtQml > $DHQML
+echo import QtQuick >> $DHQML
+echo import QtQuick.Window >> $DHQML
+echo import QtQuick.Controls >> $DHQML
+echo import QtQuick.Layouts >> $DHQML
+echo >> $DHQML
+echo Item { } >> $DHQML
+echo >> $DHQML
+
+echo module DeployHelper > $DHQMLDIR
+echo MacDeployHelper 1.0 MacDeployHelper.qml >> $DHQMLDIR
+
+$QT_LIBS_BIN_DIR/macdeployqt $APP_BUNDLE_DIR -qmldir=$DEPLOYMENT_DIR
+
+rm $DHQML
+rm $DHQMLDIR
+
---
+++ windeployhelper.bat
@@ -0,0 +1,34 @@
+# windeployhelper.bat - ease creation of deployment artifacts for Windows.
+# the `bin` subdirectory of the BUILD_OUT_DIR will contain the deployment artifacts.
+# Run this batch file from a "Developer command prompt for VS 2019".
+
+# modify the following variables to match your system's build kit settings
+SET QT_LIBS_BIN_DIR=c:\Development\Qt\6.2.0\msvc2019_64\bin
+
+# modify the following variable to point to the build output directory
+SET BUILD_OUT_DIR=%cd%\..\build-gui-v2-Desktop_Qt_6_2_0_MSVC2019_64bit-Release
+
+# don't modify anything below
+SET EXE_NAME=venus-gui-v2.exe
+SET PATH=%QT_LIBS_BIN_DIR%;%PATH%
+SET DEPLOYMENT_DIR=%BUILD_OUT_DIR%\bin
+SET DHQML=%DEPLOYMENT_DIR%\WinDeployHelper.qml
+SET DHQMLDIR=%DEPLOYMENT_DIR%\qmldir
+
+echo import QtQml > %DHQML%
+echo import QtQuick >> %DHQML%
+echo import QtQuick.Window >> %DHQML%
+echo import QtQuick.Controls >> %DHQML%
+echo import QtQuick.Layouts >> %DHQML%
+echo= >> %DHQML%
+echo Item { } >> %DHQML%
+echo= >> %DHQML%
+
+echo module DeployHelper > %DHQMLDIR%
+echo WinDeployHelper 1.0 WinDeployHelper.qml >> %DHQMLDIR%
+
+%QT_LIBS_BIN_DIR%\windeployqt.exe --compiler-runtime --qmldir %DEPLOYMENT_DIR% %DEPLOYMENT_DIR%\%EXE_NAME%
+
+del %DHQML%
+del %DHQMLDIR%
+
