ea098c57a011e062e819fe3fafa4c5dc90eca124
Matt Vogt <matthew.vogt@qinetic.com.au>
2021-12-03 14:48:01 +1000

    Add Utils.js

---
+++ components/Utils.js
@@ -0,0 +1,48 @@
+/*
+** Copyright (C) 2021 Victron Energy B.V.
+*/
+.pragma library
+
+function arrayCompare(lhs, rhs) {
+	if (!Array.isArray(lhs)) {
+		if (!Array.isArray(rhs)) {
+			return 0
+		}
+		return 1
+	} else if (!Array.isArray(rhs)) {
+		return -1
+	}
+
+	if (lhs.length < rhs.length) {
+		return -1
+	} else if (rhs.length < lhs.length) {
+		return 1
+	}
+
+	for (var i = 0; i < lhs.length; ++i) {
+		const lv = lhs[i]
+		const rv = rhs[i]
+		if (lv < rv) {
+			return -1
+		} else if (rv < lv) {
+			return 1
+		}
+	}
+
+	return 0
+}
+
+function findIndex(container, value) {
+	if (Array.isArray(container)) {
+		return container.indexof(value)
+	} else if ('count' in container) {
+		for (var i = 0; i < container.count; ++i) {
+			const e = container.get(i)
+			if (e === value) {
+				return i
+			}
+		}
+	}
+
+	return -1
+}
