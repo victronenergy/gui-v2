9588588f0ad6089c18e23c499bccd90fee9b160f
Chris Adams <chris.adams@qinetic.com.au>
2023-07-26 13:30:32 +1000

    Improve timezone handling
    Ensure that changes performed in the Settings UI cause writes to
    the appropriate backend setting value.
    
    Ensure that backend setting value changes trigger clock updates.
    
    Note that currently Qt for WebAssembly doesn't support proper
    QTimezone enumeration or tzdb.  Thus, we only support UTC or
    local timezone (i.e. timezone of the browser, not the device).
    Future resolution required, see issue #299 for more info.
    
    Contributes to issue #406

--- src/clocktime.h
+++ src/clocktime.h
@@ -15,11 +15,16 @@ class ClockTime : public QObject
 	Q_PROPERTY(QDateTime currentDateTime MEMBER m_currentDateTime NOTIFY currentDateTimeChanged)
 	Q_PROPERTY(QDateTime currentDateTimeUtc MEMBER m_currentDateTimeUtc NOTIFY currentDateTimeUtcChanged)
 	Q_PROPERTY(QString currentTimeText MEMBER m_currentTimeText NOTIFY currentTimeTextChanged)
+	Q_PROPERTY(QString currentTimeUtcText MEMBER m_currentTimeUtcText NOTIFY currentTimeUtcTextChanged)
+	Q_PROPERTY(QString systemTimeZone READ systemTimeZone WRITE setSystemTimeZone NOTIFY systemTimeZoneChanged)
 
 public:
 	ClockTime(QObject *parent);
 
 	static ClockTime* instance(QObject* parent = nullptr);
+
+	QString systemTimeZone() const;
+	void setSystemTimeZone(const QString &tz); // "region/city" format.
 
 public Q_SLOTS:
 	QString formatTime(int hour, int minute) const;
@@ -29,6 +34,8 @@ Q_SIGNALS:
 	void currentDateTimeChanged();
 	void currentDateTimeUtcChanged();
 	void currentTimeTextChanged();
+	void currentTimeUtcTextChanged();
+	void systemTimeZoneChanged();
 
 protected:
 	void timerEvent(QTimerEvent *) override;
@@ -40,6 +47,8 @@ private:
 	QDateTime m_currentDateTime;
 	QDateTime m_currentDateTimeUtc;
 	QString m_currentTimeText;
+	QString m_currentTimeUtcText;
+	QString m_systemTimeZone;
 	int m_timerId = 0;
 };
 
