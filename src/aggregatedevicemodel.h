4225c1155da9f81e00cd46e30028980f646e5b15
Bea Lam <bea.lam@qinetic.com.au>
2023-10-23 13:14:49 +1000

    Device List: add "Remove disconnected devices" button. #328
    The Device List should show devices that have been disconnected, i.e.
    those that have been removed from the source models as they are no
    longer valid.
    
    So, instead of automatically syncing the aggregate model when a source
    model removes a device, keep the device in the aggregate model, and
    remove it when the removeDisconnectedDevices() functionality is
    triggered.
    
    When a device is disconnected:
    - Show a cached copy of the device description, so that the list item
    text does not disappear.
    - Show "Not connected" as the secondary text; when the device is
    reconnected, the device status text should revert to whatever is
    appropriate for that device.

--- src/aggregatedevicemodel.h
+++ src/aggregatedevicemodel.h
@@ -14,12 +14,15 @@ class AggregateDeviceModel : public QAbstractListModel
 {
 	Q_OBJECT
 	Q_PROPERTY(int count READ count NOTIFY countChanged)
+	Q_PROPERTY(int disconnectedDeviceCount READ disconnectedDeviceCount NOTIFY disconnectedDeviceCountChanged)
 	Q_PROPERTY(QVariantList sourceModels READ sourceModels WRITE setSourceModels NOTIFY sourceModelsChanged)
 
 public:
 	enum RoleNames {
 		DeviceRole = Qt::UserRole,
-		SourceModelRole
+		SourceModelRole,
+		ConnectedRole,
+		CachedDeviceDescriptionRole
 	};
 
 	explicit AggregateDeviceModel(QObject *parent = nullptr);
@@ -29,27 +32,42 @@ public:
 	void setSourceModels(const QVariantList &models);
 
 	int count() const;
+	int disconnectedDeviceCount() const;
 
 	int rowCount(const QModelIndex &parent) const override;
 	QVariant data(const QModelIndex& index, int role) const override;
 
-	// TODO Q_INVOKABLE void removeDisconnectedDevices();
+	Q_INVOKABLE void removeDisconnectedDevices();
 
 signals:
 	void countChanged();
+	void disconnectedDeviceCountChanged();
 	void sourceModelsChanged();
 
 protected:
 	QHash<int, QByteArray> roleNames() const override;
 
 private:
-	struct DeviceInfo {
+	class DeviceInfo
+	{
+	public:
+		DeviceInfo(BaseDevice *d, BaseDeviceModel *m);
+		~DeviceInfo();
+
+		static QString infoId(BaseDevice *device, BaseDeviceModel *sourceModel);
+
+		// The id uniquely identifies the device even when it is disconnected. It is a combination
+		// of the sourceModel's modelId and the device serviceUid. (It cannot be just the device
+		// serviceUid, since e.g. a vebus device may appear in both the vebus and AC input models.)
+		QString id;
 		QPointer<BaseDevice> device;
 		QPointer<BaseDeviceModel> sourceModel;
+		QString cachedDeviceDescription;
 	};
 
 	void sourceModelRowsInserted(const QModelIndex &parent, int first, int last);
 	void sourceModelRowsAboutToBeRemoved(const QModelIndex &parent, int first, int last);
+	int indexOf(const QString &deviceInfoId) const;
 	int indexOf(const BaseDevice *device) const;
 	int insertionIndex(BaseDevice *device) const;
 	void deviceDescriptionChanged();
@@ -58,6 +76,7 @@ private:
 	QHash<int, QByteArray> m_roleNames;
 	QVector<DeviceInfo> m_deviceInfos;
 	QVariantList m_sourceModels;
+	int m_disconnectedDeviceCount = 0;
 };
 
 } /* VenusOS */
