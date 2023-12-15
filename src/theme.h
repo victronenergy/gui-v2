45aaafc4b3c1ad943d8a53f6810e28ab6a70623a
Chris Adams <chris.adams@qinetic.com.au>
2023-12-15 17:24:44 +1000

    Write a code generator for the Theme object
    Previously, the Theme object parsed the theme data at run-time
    and populated a QQmlPropertyMap with the values.
    
    This meant that any bindings or expressions which resolved
      Theme.geometry.some.value
    or similar, could not be optimized by the compiler.
    
    This commit adds a code generator which ingests the theme .json
    files and produces a strongly-typed theme object.
    
    The code generator can run either in "flat" mode (the default)
    or "nested" mode.  In flat mode, theme values will be like:
        Theme.geometry_controlCard_contentMargins
    whereas in nested mode, theme values will be like:
        Theme.geometry.controlCard.contentMargins
    
    The flat mode has the advantage that every lookup requires a
    single object resolution, and so should be more performant.

--- src/theme.h
+++ src/theme.h
@@ -6,29 +6,21 @@
 #ifndef VICTRON_VENUSOS_GUI_V2_THEME_H
 #define VICTRON_VENUSOS_GUI_V2_THEME_H
 
+#include <QGuiApplication>
+#include <QScreen>
 #include <QObject>
-#include <QtQml/qqml.h>
-
-#include <QColor>
-#include <QQmlEngine>
-#include <QQmlPropertyMap>
-#include <QObject>
-#include <QJsonValue>
-#include <QHash>
-#include <QString>
-
-#include <deque>
+#include <QSizeF>
+#include <qqmlintegration.h>
 
 namespace Victron {
 namespace VenusOS {
 
-class Theme : public QQmlPropertyMap
+class Theme : public QObject
 {
 	Q_OBJECT
-	QML_ELEMENT
-	QML_SINGLETON
-	Q_PROPERTY(ScreenSize screenSize READ screenSize NOTIFY screenSizeChanged)
-	Q_PROPERTY(ColorScheme colorScheme READ colorScheme NOTIFY colorSchemeChanged)
+	QML_NAMED_ELEMENT(ThemeBase)
+	Q_PROPERTY(ScreenSize screenSize READ screenSize WRITE setScreenSize NOTIFY screenSizeChanged)
+	Q_PROPERTY(ColorScheme colorScheme READ colorScheme WRITE setColorScheme NOTIFY colorSchemeChanged)
 
 public:
 	enum ScreenSize {
@@ -50,36 +42,45 @@ public:
 	};
 	Q_ENUM(StatusLevel)
 
-	explicit Theme(QObject *parent = nullptr);
-	~Theme() override;
+	Theme(QObject *parent = nullptr) : QObject(parent)
+	{
+#if !defined(VENUS_WEBASSEMBLY_BUILD)
+		const QSizeF physicalScreenSize = QGuiApplication::primaryScreen()->physicalSize();
+		const int screenDiagonalMm = static_cast<int>(sqrt((physicalScreenSize.width() * physicalScreenSize.width())
+			+ (physicalScreenSize.height() * physicalScreenSize.height())));
+		setScreenSize((round(screenDiagonalMm / 10 / 2.5) == 7)
+			? Victron::VenusOS::Theme::SevenInch
+			: Victron::VenusOS::Theme::FiveInch);
+#endif
+	}
 
-	ScreenSize screenSize() const;
-	ColorScheme colorScheme() const;
+	Victron::VenusOS::Theme::ScreenSize screenSize() const { return m_screenSize; }
+	void setScreenSize(Victron::VenusOS::Theme::ScreenSize size) {
+		if (m_screenSize != size) {
+			m_screenSize = size;
+			Q_EMIT screenSizeChanged(size);
+			Q_EMIT screenSizeChanged_parameterless(); // work around moc limitation.
+		}
+	}
 
-	Q_INVOKABLE bool load(ScreenSize screenSize, ColorScheme colorScheme);
-
-	Q_INVOKABLE QColor statusColorValue(StatusLevel level, bool darkColor = false) const;
+	Victron::VenusOS::Theme::ColorScheme colorScheme() const { return m_colorScheme; }
+	void setColorScheme(Victron::VenusOS::Theme::ColorScheme scheme) {
+		if (m_colorScheme != scheme) {
+			m_colorScheme = scheme;
+			Q_EMIT colorSchemeChanged(scheme);
+			Q_EMIT colorSchemeChanged_parameterless(); // work around moc limitation.
+		}
+	}
 
 Q_SIGNALS:
-	void screenSizeChanged();
-	void colorSchemeChanged();
+    void screenSizeChanged(Victron::VenusOS::Theme::ScreenSize screenSize);
+	void screenSizeChanged_parameterless();
+	void colorSchemeChanged(Victron::VenusOS::Theme::ColorScheme colorScheme);
+	void colorSchemeChanged_parameterless();
 
-private:
-	bool parseTheme(const QString &themeFile);
-	QVariant parseValue(const QJsonValue &value, const QString &key, bool defer = true);
-	void insertValue(
-		QQmlPropertyMap *tree,
-		const QString &key,
-		const QJsonValue &value,
-		int depth = 0,
-		bool defer = true);
-	QVariant resolvedValue(const QString &key, bool *found = nullptr, bool warnOnFailure = true) const;
-	QColor resolvedColor(const QString &value) const;
-
-	QHash<QString, QQmlPropertyMap *> m_subTrees;
-	ScreenSize m_screenSize = FiveInch;
+protected:
+	ScreenSize m_screenSize = SevenInch;
 	ColorScheme m_colorScheme = Dark;
-	std::deque<std::pair<QString, QJsonValue>> m_deferred;
 };
 
 }
