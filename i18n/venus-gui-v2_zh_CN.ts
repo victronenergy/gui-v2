56e8d294cd06b3894ed3e792eb90f5847f8ef126
Bea Lam <bea.lam@qinetic.com.au>
2024-02-23 10:52:48 +1000

    Enable QTranslator and lupdate to find the zh ts file
    Add the zh ts file to CMakeLists.txt so that lupdate can find it to
    generate the translations.
    
    Rename venus-gui-v2_zh-CN.ts to venus-gui-v2_zh_CN.ts, and update the
    'language' value in the ts XML to reflect this change. Translation
    loading does not work with zh-CN.
    
    Also use explicit zh_CN code for Chinese in language model to be more
    explicit about the locale in use.

--- i18n/venus-gui-v2_zh_CN.ts
+++ i18n/venus-gui-v2_zh_CN.ts
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" language="zh-CN">
+<TS version="2.1" language="zh_CN">
   <context>
     <name></name>
     <message id="ess_card_minimum_soc_value">
