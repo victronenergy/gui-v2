36839a6948f89865adcf3e361ef9f28a206f32f4
Daniel McInnes <daniel.mcinnes@qinetic.com.au>
2024-03-06 14:21:00 +1000

    Upload to poeditor works

--- scripts/upload_new_terms.sh
+++ scripts/upload_new_terms.sh
@@ -3,24 +3,27 @@
 # This script should be called _after_ building the project. Part of building the project
 # is running lupdate, and thus producing .../path/to/build/dir/i18n/venus-gui-v2.ts
 
+file_to_upload=$1
+echo "file to upload: $file_to_upload"
 if [ "${POEDITOR_TOKEN}" == "" ]
 then
   echo "Please set environment variable POEDITOR_TOKEN first"
   exit 1
 fi
 
-if [ ! -f build/i18n/venus-gui-v2.ts ]
+
+if [ ! -f $file_to_upload ]
 then
-  echo "No build/i18n/venus-gui-v2.ts found"
+  echo "File $file_to_upload not found"
   exit 1
 fi
 
-echo "### Uploading terms to POEditor"
+echo "### Uploading terms to POEditor: $file_to_upload"
 curl -X POST https://api.poeditor.com/v2/projects/upload \
      -F api_token="${POEDITOR_TOKEN}" \
      -F id="674443" \
      -F updating="terms" \
      -F sync_terms=1 \
-     -F file=@"build/i18n/venus-gui-v2.ts"
+     -F file=@"$file_to_upload"
 
 exit 0
