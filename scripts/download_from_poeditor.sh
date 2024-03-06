4d6ecf97d73ea5199858cdf1226c7776f60b58c7
Daniel McInnes <daniel.mcinnes@qinetic.com.au>
2024-03-06 14:21:00 +1000

    Upload to poeditor works

--- scripts/download_from_poeditor.sh
+++ scripts/download_from_poeditor.sh
@@ -2,27 +2,36 @@
 
 # This script is used for fetching the latest translations
 # from POEditor, making sure that the release uses the
-# most up to date translations. It is called from the GitHub
-# action, and only running on tagged releases.
+# most up to date translations. It is typically called from the GitHub
+# action, via the cmake build system, and only running on tagged releases.
+# 
+# example usage: cmake /path/to/CMakeLists.txt; make download_translations
+# example usage: download_from_poeditor.sh uk /path/to/gui-v2/i18n/venus-gui-v2_uk.ts
+
+
 if [ "${POEDITOR_TOKEN}" == "" ]
 then
   echo "Please set environment variable POEDITOR_TOKEN first"
   exit 1
 fi
 
-sed -ne 's/.*"i18n\/[^}]*}_\(.*\).ts"/\1/p' CMakeLists.txt |\
-while read -r code
-do
-  output_file="i18n/venus-gui-v2_${code}.ts"
-  echo "### Fetching ${code}, storing in ${output_file}"
-  download_url=$(curl --silent -X POST https://api.poeditor.com/v2/projects/export \
-    -d api_token="${POEDITOR_TOKEN}" \
-    -d id="674443" \
-    -d type="ts" \
-    -d language="${code}" |\
-     jq -r '.result.url')
-  curl --silent -X GET ${download_url} --output ${output_file}
+code=${1/_/-}
 
-done
+output_file=$2
+echo "### Fetching ${code}, storing in ${output_file}"
+download_url=$(curl -X POST https://api.poeditor.com/v2/projects/export \
+  -d api_token="${POEDITOR_TOKEN}" \
+  -d id="674443" \
+  -d type="ts" \
+  -d language="${code}" |\
+   jq -r '.result.url')
+
+if [ "${download_url}" == "null" ]
+then
+  echo "### Unable to determine download url for code [${code}]"
+  exit 1
+else
+  curl -X GET ${download_url} --output ${output_file}
+fi
 
 exit 0
