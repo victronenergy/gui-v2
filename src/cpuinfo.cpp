ca47d040064ec755b400dc7cff475066db965b03
Joona Petrell <joona.petrell@gmail.com>
2024-04-02 15:20:16 +0300

    Pause Overview page animations during heavy CPU load
    Contributes to #914.

---
+++ src/cpuinfo.cpp
@@ -0,0 +1,69 @@
+/*
+** Copyright (C) 2024 Victron Energy B.V.
+** See LICENSE.txt for license information.
+*/
+
+#include "cpuinfo.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+using namespace Victron::VenusOS;
+
+int get_cpu_usage(unsigned long long &previousBusy, unsigned long long &previousIdle) {
+	// Read CPU stats
+	FILE *file = fopen("/proc/stat", "r");
+	if (file == nullptr) {
+		qWarning() << "Could not open /proc/stat";
+		return -1;
+	}
+
+	unsigned long long user, nice, system, idleTotal, busyTotal;
+	fscanf(file, "cpu %llu %llu %llu %llu", &user, &nice, &system, &idleTotal);
+	busyTotal = user + nice + system;
+	fclose(file);
+
+
+	// Calculate CPU usage
+	unsigned long long busy = busyTotal - previousBusy;
+	unsigned long long idle = idleTotal - previousIdle;
+	previousBusy = busyTotal;
+	previousIdle = idleTotal;
+	return qRound((busy * 100.0) / (busy + idle));
+}
+
+CpuInfo::CpuInfo(QObject *parent)
+	: QObject(parent)
+{
+#ifdef Q_OS_LINUX
+	m_timer.setInterval(2000);
+	if (m_enabled) {
+		m_timer.start();
+	}
+
+	connect(this, &CpuInfo::enabledChanged, this, [this] {
+		if (m_enabled) {
+			m_timer.start();
+		} else {
+			m_timer.stop();
+		}
+	});
+
+	connect(&m_timer, &QTimer::timeout,
+		this, [this] {
+			int usage = get_cpu_usage(m_previousBusy, m_previousIdle);
+			if (m_usage != usage) {
+				m_usage = usage;
+				if (m_overLimit && usage < m_lowerLimit) {
+					m_overLimit = false;
+					emit overLimitChanged();
+				} else if (!m_overLimit && usage > m_upperLimit) {
+					m_overLimit = true;
+					emit overLimitChanged();
+				}
+				emit usageChanged();
+			}
+	});
+#endif
+}
---
+++ src/cpuinfo.h
@@ -0,0 +1,47 @@
+/*
+** Copyright (C) 2024 Victron Energy B.V.
+** See LICENSE.txt for license information.
+*/
+
+#ifndef CPUINFO_H
+#define CPUINFO_H
+
+#include <QObject>
+#include <QQmlEngine>
+#include <QTimer>
+
+namespace Victron {
+namespace VenusOS {
+
+class CpuInfo : public QObject
+{
+	Q_OBJECT
+	QML_ELEMENT
+	Q_PROPERTY(int usage MEMBER m_usage NOTIFY usageChanged)
+	Q_PROPERTY(bool enabled MEMBER m_enabled NOTIFY enabledChanged)
+	Q_PROPERTY(int upperLimit MEMBER m_upperLimit NOTIFY upperLimitChanged)
+	Q_PROPERTY(int lowerLimit MEMBER m_lowerLimit NOTIFY lowerLimitChanged)
+	Q_PROPERTY(bool overLimit MEMBER m_overLimit NOTIFY overLimitChanged)
+public:
+	explicit CpuInfo(QObject *parent = nullptr);
+Q_SIGNALS:
+	void usageChanged();
+	void enabledChanged();
+	void upperLimitChanged();
+	void lowerLimitChanged();
+	void overLimitChanged();
+private:
+	int m_usage = 0;
+	int m_lowerLimit = 0;
+	int m_upperLimit = 100;
+	bool m_overLimit = false;
+	bool m_enabled = true;
+	unsigned long long m_previousBusy = 0;
+	unsigned long long m_previousIdle = 0;
+	QTimer m_timer;
+};
+
+}
+}
+
+#endif // CPUINFO_H
