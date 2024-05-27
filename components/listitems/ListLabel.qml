victronenergy/gui-v2

commit f04e21956e32cad033640ed05a40b02b4ec5864e

Allow PageAcInSetup to be loaded
The ListLabel was referring to an 'allowed' property, which does not
exist in ListLabel.

Add an 'allowed' property to ListLabel, similar to the one in
ListItem, which can be used to show/hide the item without doing a
re-layout on the ListView.

Fixes #1115

components/listitems/ListLabel.qml
@@ -7,10 +7,13 @@ import QtQuick
 import Victron.VenusOS
 
 Label {
+	property bool allowed
+
 	width: parent ? parent.width : 0
-	height: visible ? implicitHeight : 0
-	topPadding: visible ? Theme.geometry_listItem_content_verticalMargin : 0
-	bottomPadding: visible ? Theme.geometry_listItem_content_verticalMargin : 0
+	height: allowed ? implicitHeight : 0
+	visible: allowed
+	topPadding: allowed ? Theme.geometry_listItem_content_verticalMargin : 0
+	bottomPadding: allowed ? Theme.geometry_listItem_content_verticalMargin : 0
 	leftPadding: Theme.geometry_listItem_content_horizontalMargin
 	rightPadding: Theme.geometry_listItem_content_horizontalMargin
 	font.pixelSize: Theme.font_size_body1

commit 08eaaba26c3fe06dad9aa5a435aee8eeedef56fc

ListLabel should be visible by default
Regression from f04e21956e32cad033640ed05a40b02b4ec5864e

Fixes #1140

components/listitems/ListLabel.qml
@@ -7,7 +7,7 @@ import QtQuick
 import Victron.VenusOS
 
 Label {
-	property bool allowed
+	property bool allowed: true
 
 	width: parent ? parent.width : 0
 	height: allowed ? implicitHeight : 0