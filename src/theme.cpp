85a56b4a5e32d7c56725e6a43c07bcbe120e89f5
Joona Petrell <joona.petrell@gmail.com>
2024-06-03 11:47:35 +0300

    Use 5" inch layout on small mobile screens (wasm)
    Cannot declare EM_JS macro on the header as that will result in duplicate symbols error
    as any place that includes the header will have own version of the same global function.
    
    Fixes #919.

---
+++ src/theme.cpp
@@ -0,0 +1,72 @@
+/*
+** Copyright (C) 2024 Victron Energy B.V.
+** See LICENSE.txt for license information.
+*/
+
+#include "theme.h"
+
+using namespace Victron::VenusOS;
+
+#if defined(VENUS_WEBASSEMBLY_BUILD)
+#include <emscripten.h>
+
+EM_JS(int, getScreenWidth, (), {
+  return Math.min(screen.width, screen.height);
+});
+
+#endif
+
+Theme::Theme(QObject *parent) : QObject(parent)
+{
+#if defined(VENUS_WEBASSEMBLY_BUILD)
+	// 5"-6" Smartphones have 320 - 480 CSS independent pixel wide screens.
+	setScreenSize(getScreenWidth() >= 480
+				  ? Victron::VenusOS::Theme::SevenInch
+				  : Victron::VenusOS::Theme::FiveInch);
+#else
+	const QSizeF physicalScreenSize = QGuiApplication::primaryScreen()->physicalSize();
+	const int screenDiagonalMm = static_cast<int>(sqrt((physicalScreenSize.width() * physicalScreenSize.width())
+		+ (physicalScreenSize.height() * physicalScreenSize.height())));
+	setScreenSize((round(screenDiagonalMm / 10 / 2.5) == 7)
+		? Victron::VenusOS::Theme::SevenInch
+		: Victron::VenusOS::Theme::FiveInch);
+#endif
+}
+
+Victron::VenusOS::Theme::ScreenSize Theme::screenSize() const
+{
+	return m_screenSize;
+}
+
+void Theme::setScreenSize(Victron::VenusOS::Theme::ScreenSize size)
+{
+	if (m_screenSize != size) {
+		m_screenSize = size;
+		Q_EMIT screenSizeChanged(size);
+		Q_EMIT screenSizeChanged_parameterless(); // work around moc limitation.
+	}
+}
+
+Victron::VenusOS::Theme::ColorScheme Theme::colorScheme() const
+{
+	return m_colorScheme;
+}
+
+void Theme::setColorScheme(Victron::VenusOS::Theme::ColorScheme scheme)
+{
+	if (m_colorScheme != scheme) {
+		m_colorScheme = scheme;
+		Q_EMIT colorSchemeChanged(scheme);
+		Q_EMIT colorSchemeChanged_parameterless(); // work around moc limitation.
+	}
+}
+
+Q_INVOKABLE bool Theme::objectHasQObjectParent(QObject *obj) const
+{
+	return obj && obj->parent();
+}
+
+QString Theme::applicationVersion() const
+{
+	return QStringLiteral("v%1.%2.%3").arg(PROJECT_VERSION_MAJOR).arg(PROJECT_VERSION_MINOR).arg(PROJECT_VERSION_PATCH);
+}
