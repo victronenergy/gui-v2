victronenergy/gui-v2

commit a9038b76bfddc53ebc3c3db55061ad4bd671556a

Clean up phase models in EvCharger and PvInverter
Use a phase count property that is set to the number of phases with
valid values, so that the EvCharger and PvInverter pages do not show
more than this number of phases in the phase tables.

Also, expose the phase objects Instantiator instead of using a
separate ListModel for phase data, as the VeQuickItem objects need
to be created anyway so the ListModel doesn't add anything useful.

data/common/PvInverter.qml
@@ -17,61 +17,44 @@ Device {
 	readonly property real power: _power.isValid ? _power.value : NaN
 	readonly property real voltage: _voltage.isValid ? _voltage.value : NaN
 
-	readonly property ListModel phases: ListModel {
-		function setPhaseProperty(phaseName, propertyName, value) {
-			let index = count
-			let needsInsert = true
-			for (let i = 0; i < count; ++i) {
-				const currName = get(i).name
-				if (currName === phaseName) {
-					needsInsert = false
-					index = i
-					break
-				} else if (currName > phaseName) {
-					// Do an ordered insertion.
-					index = i
-					break
-				}
-			}
-			if (value === undefined) {
-				value = NaN
-			}
-			if (needsInsert) {
-				if (isNaN(value)) {
-					// Wait until we have a valid value for this phase before adding it to the model
-					return
-				}
-				let data = { name: phaseName, energy: NaN, power: NaN, current: NaN, voltage: NaN }
-				data[propertyName] = value
-				insert(index, data)
-			} else if (index >= 0 && index < count) {
-				setProperty(index, propertyName, value)
-			} else {
-				console.warn("setPhaseProperty(): bad index", index, "count is", count)
-			}
+	readonly property QtObject phases: QtObject {
+		property int count
+
+		function updateCount(maxPhaseCount) {
+			count = Math.max(count, maxPhaseCount)
 		}
 
-		readonly property Instantiator _phaseObjects: Instantiator {
+		function get(index) {
+			return _phases.objectAt(index)
+		}
+
+		readonly property Instantiator _phases: Instantiator {
 			model: 3
 			delegate: QtObject {
-				readonly property string phaseName: "L" + (model.index + 1)
-				readonly property string phaseUid: pvInverter.serviceUid + "/Ac/" + phaseName
+				required property int index
+				readonly property string phaseUid: pvInverter.serviceUid + "/Ac/L" + (index + 1)
+
+				readonly property string name: "L" + (index + 1)
+				readonly property real energy: _phaseEnergy.isValid ? _phaseEnergy.value : NaN
+				readonly property real power: _phasePower.isValid ? _phasePower.value : NaN
+				readonly property real current: _phaseCurrent.isValid ? _phaseCurrent.value : NaN
+				readonly property real voltage: _phaseVoltage.isValid ? _phaseVoltage.value : NaN
 
 				readonly property VeQuickItem _phaseEnergy: VeQuickItem {
 					uid: phaseUid + "/Energy/Forward"
-					onValueChanged: phases.setPhaseProperty(phaseName, "energy", value)
+					onIsValidChanged: if (isValid) phases.updateCount(index + 1)
 				}
 				readonly property VeQuickItem _phasePower: VeQuickItem {
 					uid: phaseUid + "/Power"
-					onValueChanged: phases.setPhaseProperty(phaseName, "power", value)
+					onIsValidChanged: if (isValid) phases.updateCount(index + 1)
 				}
 				readonly property VeQuickItem _phaseCurrent: VeQuickItem {
 					uid: phaseUid + "/Current"
-					onValueChanged: phases.setPhaseProperty(phaseName, "current", value)
+					onIsValidChanged: if (isValid) phases.updateCount(index + 1)
 				}
 				readonly property VeQuickItem _phaseVoltage: VeQuickItem {
 					uid: phaseUid + "/Voltage"
-					onValueChanged: phases.setPhaseProperty(phaseName, "voltage", value)
+					onIsValidChanged: if (isValid) phases.updateCount(index + 1)
 				}
 			}
 		}

data/common/EvCharger.qml
@@ -20,37 +20,39 @@ Device {
 	readonly property real current: _current.isValid ? _current.value : NaN
 	readonly property real maxCurrent: _maxCurrent.isValid ? _maxCurrent.value : NaN
 
-	readonly property ListModel phases: ListModel {
-		function setPower(index, value) {
-			if (index >= 0 && index < count) {
-				setProperty(index, "power", value === undefined ? NaN : value)
-			}
+	readonly property QtObject phases: QtObject {
+		property int count
+
+		function updateCount(maxPhaseCount) {
+			count = Math.max(count, maxPhaseCount)
 		}
 
-		Component.onCompleted: {
-			const properties = [_phase1Power, _phase2Power, _phase3Power]
-			for (let i = 0; i < properties.length; ++i) {
-				const v = properties[i].value
-				append({ name: "L" + (i + 1), power: v === undefined ? NaN : v })
+		function get(index) {
+			return _phases.objectAt(index)
+		}
+
+		readonly property Instantiator _phases: Instantiator {
+			model: 3
+			delegate: QtObject {
+				required property int index
+				readonly property string phaseUid: evCharger.serviceUid + "/Ac/L" + (index + 1)
+				readonly property string name: "L" + (index + 1)
+				readonly property real power: _power.isValid ? _power.value : NaN
+
+				function updatePhaseCount(phaseCount) {
+					evCharger.count = Math.max(evCharger.count, phaseCount)
+				}
+
+				readonly property VeQuickItem _power: VeQuickItem {
+					uid: phaseUid + "/Power"
+					onIsValidChanged: if (isValid) phases.updateCount(index + 1)
+				}
 			}
 		}
 	}
 
 	readonly property VeQuickItem _energy: VeQuickItem {
 		uid: evCharger.serviceUid + "/Ac/Energy/Forward"
-	}
-
-	readonly property VeQuickItem _phase1Power: VeQuickItem {
-		uid: evCharger.serviceUid + "/Ac/L1/Power"
-		onValueChanged: phases.setPower(0, value)
-	}
-	readonly property VeQuickItem _phase2Power: VeQuickItem {
-		uid: evCharger.serviceUid + "/Ac/L2/Power"
-		onValueChanged: phases.setPower(1, value)
-	}
-	readonly property VeQuickItem _phase3Power: VeQuickItem {
-		uid: evCharger.serviceUid + "/Ac/L3/Power"
-		onValueChanged: phases.setPower(2, value)
 	}
 
 	readonly property VeQuickItem _power: VeQuickItem {

data/mock/EvChargersImpl.qml
@@ -63,9 +63,9 @@ QtObject {
 					const phase1Power = zeroPower ? 0 : Math.random() * 50
 					const phase2Power = zeroPower ? 0 : Math.random() * 50
 					const phase3Power = zeroPower ? 0 : Math.random() * 50
-					_phase1Power.setValue(phase1Power)
-					_phase2Power.setValue(phase2Power)
-					_phase3Power.setValue(phase3Power)
+					phases.get(0)._power.setValue(phase1Power)
+					phases.get(1)._power.setValue(phase2Power)
+					phases.get(2)._power.setValue(phase3Power)
 
 					_energy.setValue(1 + Math.random() * 10)
 					_current.setValue(1 + Math.random() * 20)

data/mock/PvInvertersImpl.qml
@@ -77,13 +77,6 @@ QtObject {
 				_deviceInstance.setValue(deviceInstance)
 				_customName.setValue("My PV Inverter " + deviceInstance)
 				_statusCode.setValue(Math.random() * VenusOS.PvInverter_StatusCode_Error)
-
-				for (let phaseIndex = 0; phaseIndex < 3; ++phaseIndex) {
-					Global.mockDataSimulator.setMockValue(serviceUid + "/Ac/L%1/Power".arg(phaseIndex + 1), NaN)
-					Global.mockDataSimulator.setMockValue(serviceUid + "/Ac/L%1/Current".arg(phaseIndex + 1), NaN)
-					Global.mockDataSimulator.setMockValue(serviceUid + "/Ac/L%1/Voltage".arg(phaseIndex + 1), NaN)
-					Global.mockDataSimulator.setMockValue(serviceUid + "/Ac/L%1/Energy/Forward".arg(phaseIndex + 1), NaN)
-				}
 			}
 		}
 	}

commit e94dbc44bfc4c9f694726f86de5bb45d5df32b76

Overview solar: fix multi-phase display for PV inverters
There are two use cases for showing PV inverter multi-phase data:

1) Overview SolarYieldWidget, which shows a ThreePhaseDisplay for the
PV inverter. ThreePhaseDisplay needs a QAbstractItemModel-based model
to create the display delegates using the model roles.

2) PvInverterPage, which needs a QObject for each phase to create a
property binding to each phase measurement property (power, current,
etc.) so that valueForModelIndex() will be invoked when a measurement
value changes.

Change PvInverter to use PhaseModel provide the QAbstractItemModel-
based model for case 1, and also provide a getPhase() function for
case 2.

QuantityTable could be reworked in future to avoid the need for a
valueForModelIndex() function that requires QObject-bindings, thereby
removing the need for case 2.

Fixes the regression from a9038b76bfddc53ebc3c3db55061ad4bd671556a.
EvCharger does not need a similar fix, since the QAbstractItemModel-
based model is not required for current EVCS features in the UI.

Fixes #1654

data/common/PvInverter.qml
@@ -17,14 +17,14 @@ Device {
 	readonly property real power: _power.isValid ? _power.value : NaN
 	readonly property real voltage: _voltage.isValid ? _voltage.value : NaN
 
-	readonly property QtObject phases: QtObject {
-		property int count
-
+	readonly property PhaseModel phases: PhaseModel {
 		function updateCount(maxPhaseCount) {
-			count = Math.max(count, maxPhaseCount)
+			// pvinverter services do not have /NumberOfPhases, so manually update the phase model
+			// count when phase measurements are detected.
+			phaseCount = Math.max(count, maxPhaseCount)
 		}
 
-		function get(index) {
+		function getPhase(index) {
 			return _phases.objectAt(index)
 		}
 
@@ -43,18 +43,22 @@ Device {
 				readonly property VeQuickItem _phaseEnergy: VeQuickItem {
 					uid: phaseUid + "/Energy/Forward"
 					onIsValidChanged: if (isValid) phases.updateCount(index + 1)
+					onValueChanged: phases.setValue(index, PhaseModel.EnergyRole, value)
 				}
 				readonly property VeQuickItem _phasePower: VeQuickItem {
 					uid: phaseUid + "/Power"
 					onIsValidChanged: if (isValid) phases.updateCount(index + 1)
+					onValueChanged: phases.setValue(index, PhaseModel.PowerRole, value)
 				}
 				readonly property VeQuickItem _phaseCurrent: VeQuickItem {
 					uid: phaseUid + "/Current"
 					onIsValidChanged: if (isValid) phases.updateCount(index + 1)
+					onValueChanged: phases.setValue(index, PhaseModel.CurrentRole, value)
 				}
 				readonly property VeQuickItem _phaseVoltage: VeQuickItem {
 					uid: phaseUid + "/Voltage"
 					onIsValidChanged: if (isValid) phases.updateCount(index + 1)
+					onValueChanged: phases.setValue(index, PhaseModel.VoltageRole, value)
 				}
 			}
 		}

pages/solar/PvInverterPage.qml
@@ -69,7 +69,7 @@ Page {
 						{ title: CommonWords.power_watts, unit: VenusOS.Units_Watt }
 					]
 					valueForModelIndex: function(phaseIndex, column) {
-						const phase = root.pvInverter.phases.get(phaseIndex)
+						const phase = root.pvInverter.phases.getPhase(phaseIndex)
 						const columnProperties = ["name", "energy", "voltage", "current", "power"]
 						return phase[columnProperties[column]]
 					}

src/phasemodel.cpp
@@ -66,6 +66,14 @@ void PhaseModel::setValue(int index, Role role, const qreal value)
 		phase.current = value;
 		emit dataChanged(createIndex(index, 0), createIndex(index, 0), { CurrentRole });
 		break;
+	case VoltageRole:
+		phase.voltage = value;
+		emit dataChanged(createIndex(index, 0), createIndex(index, 0), { VoltageRole });
+		break;
+	case EnergyRole:
+		phase.energy = value;
+		emit dataChanged(createIndex(index, 0), createIndex(index, 0), { EnergyRole });
+		break;
 	default:
 		break;
 	}
@@ -101,6 +109,10 @@ QVariant PhaseModel::data(const QModelIndex &index, int role) const
 		return phase.power;
 	case CurrentRole:
 		return phase.current;
+	case VoltageRole:
+		return phase.voltage;
+	case EnergyRole:
+		return phase.energy;
 	default:
 		return QVariant();
 	}
@@ -133,7 +145,9 @@ QHash<int, QByteArray> PhaseModel::roleNames() const
 	static QHash<int, QByteArray> roles = {
 		{ NameRole, "name" },
 		{ PowerRole, "power" },
-		{ CurrentRole, "current" }
+		{ CurrentRole, "current" },
+		{ VoltageRole, "voltage" },
+		{ EnergyRole, "energy" }
 	};
 	return roles;
 }

src/phasemodel.h
@@ -24,7 +24,9 @@ public:
 	enum Role {
 		NameRole = Qt::UserRole,
 		PowerRole,
-		CurrentRole
+		CurrentRole,
+		VoltageRole,
+		EnergyRole
 	};
 	Q_ENUM(Role)
 
@@ -56,8 +58,10 @@ protected:
 
 private:
 	struct Phase {
-		qreal power = 0;
-		qreal current = 0;
+		qreal power = qQNaN();
+		qreal current = qQNaN();
+		qreal voltage = qQNaN();
+		qreal energy = qQNaN();
 	};
 
 	void resetModel();