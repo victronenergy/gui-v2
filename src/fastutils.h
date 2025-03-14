f2a8c377c0f3f9d3d041811aeeab422bc8d98d54
Chris Adams <chris.adams@qinetic.com.au>
2025-03-14 16:56:28 +1000

    Reduce work done to recalculate load graph paths

---
+++ src/fastutils.h
@@ -0,0 +1,37 @@
+/*
+** Copyright (C) 2025 Victron Energy B.V.
+** See LICENSE.txt for license information.
+*/
+
+#ifndef VICTRON_VENUSOS_GUI_V2_FASTUTILS_H
+#define VICTRON_VENUSOS_GUI_V2_FASTUTILS_H
+
+#include <QQmlEngine>
+#include <QObject>
+
+namespace Victron {
+namespace VenusOS {
+
+/*
+** Similar to Utils.js but in C++ so it's much faster.
+** Obviously, we cannot do JavaScript-specific mutations here,
+** but for many use-cases, it's far more efficient to define them here.
+*/
+
+class FastUtils : public QObject
+{
+	Q_OBJECT
+	QML_ELEMENT
+	QML_SINGLETON
+
+public:
+	explicit FastUtils(QObject *parent = nullptr);
+	~FastUtils() override;
+
+	Q_INVOKABLE QList<qreal> calculateLoadGraphYValues(const QList<qreal> &data, int dataLen, qreal height) const;
+};
+
+}
+}
+
+#endif // VICTRON_VENUSOS_GUI_V2_FASTUTILS_H
