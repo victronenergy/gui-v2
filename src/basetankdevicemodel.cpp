ac2327b487ed32c2bf1860a8a6ad6d14fdcb451f
Bea Lam <bea.lam@qinetic.com.au>
2025-03-27 08:16:09 +1000

    Move tank total calculations into BaseTankDeviceModel
    Since each BaseTankDeviceModel has its own list of tanks, it can
    automatically update the totals when necessary.
    
    Part of #1992

--- src/basetankdevicemodel.cpp
+++ src/basetankdevicemodel.cpp
@@ -11,6 +11,8 @@ using namespace Victron::VenusOS;
 BaseTankDeviceModel::BaseTankDeviceModel(QObject *parent)
 	: BaseDeviceModel(parent)
 {
+	connect(this, &BaseDeviceModel::rowsInserted, this, &BaseTankDeviceModel::modelRowsInserted);
+	connect(this, &BaseDeviceModel::rowsRemoved, this, &BaseTankDeviceModel::modelRowsRemoved);
 }
 
 int BaseTankDeviceModel::type() const
@@ -68,4 +70,77 @@ void BaseTankDeviceModel::setTotalRemaining(qreal totalRemaining)
 BaseTankDevice *BaseTankDeviceModel::tankAt(int index) const
 {
 	return qobject_cast<BaseTankDevice*>(deviceAt(index));
+}
+
+void BaseTankDeviceModel::modelRowsInserted(const QModelIndex &parent, int first, int last)
+{
+	for (int i = first; i <= last; ++i) {
+		// Update the totals whenever the measurements change for each tank. A device typically
+		// changes all of these values at the same time for each device, so delay the update until
+		// the end of the event loop to minimize unnecessary recalculations.
+		if (BaseTankDevice *tank = tankAt(i)) {
+			connect(tank, &BaseTankDevice::levelChanged,
+					this, &BaseTankDeviceModel::updateTotals,
+					Qt::QueuedConnection);
+			connect(tank, &BaseTankDevice::capacityChanged,
+					this, &BaseTankDeviceModel::updateTotals,
+					Qt::QueuedConnection);
+			connect(tank, &BaseTankDevice::remainingChanged,
+					this, &BaseTankDeviceModel::updateTotals,
+					Qt::QueuedConnection);
+		}
+	}
+	updateTotals();
+}
+
+void BaseTankDeviceModel::modelRowsRemoved(const QModelIndex &parent, int first, int last)
+{
+	for (int i = first; i <= last; ++i) {
+		if (BaseTankDevice *tank = tankAt(i)) {
+			tank->disconnect(this);
+		}
+	}
+	updateTotals();
+}
+
+// TODO move this to a common place with Units::sumRealNumbers()
+qreal BaseTankDeviceModel::sumOf(qreal a, qreal b) const
+{
+	const bool aNaN = qIsNaN(a);
+	const bool bNaN = qIsNaN(b);
+
+	return (aNaN && bNaN) ? qQNaN()
+		: aNaN ? b
+		: bNaN ? a
+		: (a+b);
+}
+
+void BaseTankDeviceModel::updateTotals()
+{
+	qreal totalLevel = qQNaN();
+	qreal totalCapacity = qQNaN();
+	qreal totalRemaining = qQNaN();
+	bool requireFallback = false;
+
+	for (int i = 0; i < count(); ++i) {
+		if (const BaseTankDevice *tank = tankAt(i)) {
+			totalLevel = sumOf(totalLevel, tank->level());
+			totalCapacity = sumOf(totalCapacity, tank->capacity());
+			totalRemaining = sumOf(totalRemaining, tank->remaining());
+			if (!requireFallback) {
+				requireFallback = qIsNaN(tank->remaining()) || qIsNaN(tank->capacity());
+			}
+		}
+	}
+
+	setTotalCapacity(totalCapacity);
+	setTotalRemaining(totalRemaining);
+
+	if (!requireFallback && !qIsNaN(totalRemaining) && !qIsNaN(totalCapacity) && totalCapacity > 0) {
+		// if we know all tank capacities and usages, we can calculate the combined level.
+		setAverageLevel(totalRemaining / totalCapacity * 100);
+	} else {
+		// only fall back to a crude average level if we don't know all tank capacities and usages.
+		setAverageLevel(qIsNaN(totalLevel) || count() == 0 ? qQNaN() : totalLevel / count());
+	}
 }
