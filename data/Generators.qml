victronenergy/gui-v2

commit 6610c08038c0acf38c89eabbdb7552127aa5e588

Change ServiceDeviceModel to accept multiple service types
This means MultiServiceDeviceModel can be removed.

Note device delegates with multiple conditions that determine
whether the delegate should be added to the model, an additional
addedToModel flag is required, else the 'add' condition may be
triggered multiple times and it may attempt to add the device even
when it has already been added to the model.

data/Generators.qml
@@ -10,7 +10,7 @@ QtObject {
 	id: root
 
 	readonly property ServiceDeviceModel model: ServiceDeviceModel {
-		serviceType: "generators"
+		serviceTypes: ["generators"]
 		modelId: "generators"
 		sortBy: BaseDeviceModel.SortByDeviceInstance
 	}

CMakeLists.txt
@@ -224,7 +224,6 @@ set (VENUS_QML_MODULE_SOURCES
     components/LoadGraph.qml
     components/LoadGraphShapePath.qml
     components/KeyNavigationListHelper.qml
-    components/MultiServiceDeviceModel.qml
     components/NavBar.qml
     components/NavButton.qml
     components/NetworkServices.qml

components/AllDevicesModel.qml
@@ -40,37 +40,37 @@ AggregateDeviceModel {
 	].concat(Global.tanks.allTankModels)
 
 	readonly property ServiceDeviceModel acLoadDevices: ServiceDeviceModel {
-		serviceType: "acload"
+		serviceTypes: ["acload"]
 		modelId: "acload"
 	}
 
 	readonly property ServiceDeviceModel batteryDevices: ServiceDeviceModel {
-		serviceType: "battery"
+		serviceTypes: ["battery"]
 		modelId: "battery"
 	}
 
-	readonly property MultiServiceDeviceModel combinedDcLoadDevices: MultiServiceDeviceModel {
+	readonly property ServiceDeviceModel combinedDcLoadDevices: ServiceDeviceModel {
 		serviceTypes: ["dcload", "dcsystem", "dcdc"]
 		modelId: "combinedDcloads"
 	}
 
 	readonly property ServiceDeviceModel digitalInputDevices: ServiceDeviceModel {
-		serviceType: "digitalinput"
+		serviceTypes: ["digitalinput"]
 		modelId: "digitalinput"
 	}
 
 	readonly property ServiceDeviceModel gpsDevices: ServiceDeviceModel {
-		serviceType: "gps"
+		serviceTypes: ["gps"]
 		modelId: "gps"
 	}
 
 	readonly property ServiceDeviceModel gridDevices: ServiceDeviceModel {
-		serviceType: "grid"
+		serviceTypes: ["grid"]
 		modelId: "grid"
 	}
 
 	readonly property ServiceDeviceModel gensetDevices: ServiceDeviceModel {
-		serviceType: "genset"
+		serviceTypes: ["genset"]
 		modelId: "genset"
 	}
 
@@ -83,32 +83,32 @@ AggregateDeviceModel {
 	}
 
 	readonly property ServiceDeviceModel meteoDevices: ServiceDeviceModel {
-		serviceType: "meteo"
+		serviceTypes: ["meteo"]
 		modelId: "meteo"
 	}
 
 	readonly property ServiceDeviceModel motorDriveDevices: ServiceDeviceModel {
-		serviceType: "motordrive"
+		serviceTypes: ["motordrive"]
 		modelId: "motordrive"
 	}
 
 	readonly property ServiceDeviceModel pulseMeterDevices: ServiceDeviceModel {
-		serviceType: "pulsemeter"
+		serviceTypes: ["pulsemeter"]
 		modelId: "pulsemeter"
 	}
 
 	readonly property ServiceDeviceModel solarChargerDevices: ServiceDeviceModel {
-		serviceType: "solarcharger"
+		serviceTypes: ["solarcharger"]
 		modelId: "solarcharger"
 	}
 
 	readonly property ServiceDeviceModel switchDevices: ServiceDeviceModel {
-		serviceType: "switch"
+		serviceTypes: ["switch"]
 		modelId: "switch"
 	}
 
 	readonly property ServiceDeviceModel unsupportedDevices: ServiceDeviceModel {
-		serviceType: "unsupported"
+		serviceTypes: ["unsupported"]
 		modelId: "unsupported"
 	}
 }

components/HeatPumpModel.qml
@@ -11,7 +11,7 @@ ServiceDeviceModel {
 
 	required property int position
 
-	serviceType: "heatpump"
+	serviceTypes: ["heatpump"]
 	modelId: "heatpump"
 
 	deviceDelegate: Device {
@@ -19,6 +19,7 @@ ServiceDeviceModel {
 
 		required property string uid
 		readonly property bool positionMatched: valid && _position.valid && _position.value === root.position
+		property bool addedToModel
 
 		readonly property VeQuickItem _position: VeQuickItem {
 			uid: device.serviceUid + "/Position"
@@ -26,10 +27,12 @@ ServiceDeviceModel {
 
 		serviceUid: uid
 		onPositionMatchedChanged: {
-			if (positionMatched) {
+			if (positionMatched && !addedToModel) {
 				root.addDevice(device)
-			} else {
+				addedToModel = true
+			} else if (!positionMatched && addedToModel) {
 				root.removeDevice(device.serviceUid)
+				addedToModel = false
 			}
 		}
 	}

components/MultiServiceDeviceModel.qml
@@ -1,37 +1,0 @@
-/*
-** Copyright (C) 2024 Victron Energy B.V.
-** See LICENSE.txt for license information.
-*/
-
-import QtQuick
-import Victron.VenusOS
-
-// Provides a generic DeviceModel for a set of service types.
-//
-// All services found for the specified types will be added and removed as Device objects.
-
-DeviceModel {
-	id: root
-
-	required property var serviceTypes
-
-	readonly property Instantiator _serviceObjects: Instantiator {
-		model: root.serviceTypes
-		delegate: Instantiator {
-			required property string modelData
-
-			model: ServiceModel { serviceTypes: [modelData] }
-			delegate: Device {
-				id: device
-				serviceUid: model.uid
-				onValidChanged: {
-					if (valid) {
-						root.addDevice(device)
-					} else {
-						root.removeDevice(device.serviceUid)
-					}
-				}
-			}
-		}
-	}
-}

components/ServiceDeviceModel.qml
@@ -6,29 +6,39 @@
 import QtQuick
 import Victron.VenusOS
 
-// Provides a generic DeviceModel for a specified service type.
+// Provides a generic DeviceModel for the specified service types.
 //
-// All services found for the specified type will be added and removed as Device objects.
+// All services found for the specified types will be added and removed as Device objects.
 
 DeviceModel {
 	id: root
 
-	required property string serviceType
+	required property list<string> serviceTypes
 
-	property Component deviceDelegate: Device {
-		id: device
-		serviceUid: model.uid
-		onValidChanged: {
-			if (valid) {
-				root.addDevice(device)
-			} else {
-				root.removeDevice(device.serviceUid)
+	property Component deviceDelegate: _defaultComponent
+
+	readonly property Component _defaultComponent: Component {
+		id: defaultComponent
+		Device {
+			id: device
+			serviceUid: model.uid
+			onValidChanged: {
+				if (valid) {
+					root.addDevice(device)
+				} else {
+					root.removeDevice(device.serviceUid)
+				}
 			}
 		}
 	}
 
 	readonly property Instantiator _objects: Instantiator {
-		model: ServiceModel { serviceTypes: [root.serviceType] }
+		model: serviceModel
 		delegate: root.deviceDelegate
+	}
+
+	readonly property ServiceModel _serviceModel: ServiceModel {
+		id: serviceModel
+		serviceTypes: root.serviceTypes
 	}
 }

components/ServiceModel.qml
@@ -27,7 +27,7 @@ VeQItemSortTableModel {
 	dynamicSortFilter: BackendConnection.type !== BackendConnection.MqttSource
 	filterRole: VeQItemTableModel.UniqueIdRole
 	filterRegExp: BackendConnection.type === BackendConnection.MqttSource ? ""
-			: "^%1/com\.victronenergy\.(?:%2)\."
+			: "^%1/com\\.victronenergy\\.(?:%2)\\."
 					.arg(BackendConnection.uidPrefix())
 					.arg(root.serviceTypes.join("|"))
 	model: BackendConnection.type === BackendConnection.MqttSource ? mqttModel : Global.dataServiceModel

data/EnvironmentInputs.qml
@@ -10,7 +10,7 @@ QtObject {
 	id: root
 
 	readonly property ServiceDeviceModel model: ServiceDeviceModel {
-		serviceType: "temperature"
+		serviceTypes: ["temperature"]
 		modelId: "temperature"
 	}
 

commit 45121d5f972aa112b268e077c2cdcb839f788dd0

Ensure generators are controllable from settings page
Fix Global.generators.model to use the correct service uid, which is
'generator' instead of 'generators'. This was causing a bug where
GensetStartStop1Finder would not find the available generators.

Regression from 6610c08038c0acf38c89eabbdb7552127aa5e588.

Fixes #2317

data/Generators.qml
@@ -10,8 +10,8 @@ QtObject {
 	id: root
 
 	readonly property ServiceDeviceModel model: ServiceDeviceModel {
-		serviceTypes: ["generators"]
-		modelId: "generators"
+		serviceTypes: ["generator"]
+		modelId: "generator"
 		sortBy: BaseDeviceModel.SortByDeviceInstance
 	}
 