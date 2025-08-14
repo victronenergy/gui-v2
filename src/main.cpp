71d64d47113c7d2dbbee0b83613351a798c4cfc7
Chris Adams <chris.adams@qinetic.com.au>
2025-08-14 21:38:43 +1000

    Enable multisample buffers for new hardware
    This partially resolves (or perhaps masks) the rendering issue
    seen on the new hardware.
    
    Contributes to issue #2416

--- src/main.cpp
+++ src/main.cpp
@@ -488,6 +488,12 @@ int main(int argc, char *argv[])
 
 	QScopedPointer<QObject> object(component.beginCreate(engine.rootContext()));
 	const auto window = qobject_cast<QQuickWindow *>(object.data());
+#if defined(VENUS_DESKTOP_BUILD) || defined(VENUS_GX_BUILD_AARCH64) || defined(VENUS_WEBASSEMBLY_BUILD)
+	// CerboGX doesn't support multisample render buffers; other platforms do.
+	QSurfaceFormat format = window->format();
+	format.setSamples(4); // enable MSAA
+	window->setFormat(format);
+#endif
 
 #if defined(VENUS_WEBASSEMBLY_BUILD)
 	QObject::connect(window, &QQuickWindow::activeFocusItemChanged, [window] {
@@ -505,11 +511,6 @@ int main(int argc, char *argv[])
 	fpsCounter->setWindow(window);
 	fpsCounter->setEnabled(enableFpsCounter);
 
-#if defined(VENUS_DESKTOP_BUILD)
-	QSurfaceFormat format = window->format();
-	format.setSamples(4); // enable MSAA
-	window->setFormat(format);
-#endif
 	engine.setIncubationController(window->incubationController());
 
 	/* Write to window properties here to perform any additional initialization
