victronenergy/gui-v2

commit 0a7e80ec6d6503fb343056eecb6b763f722ea953

Switch pane: implement Unranged Setpoint control
Add MiniSpinBox to provide a SpinBox-type control for switchable
outputs of the 'Unranged setpoint' type. The control is configured by:

- /SwitchableOutput/<x>/Dimming - the displayed value
- /SwitchableOutput/<x>/Settings/<DimmingMin|DimmingMax|StepSize> - the
min, max and step size for the spin box (optional)
- /SwitchableOutput/<x>/Settings/Unit - a unit to be displayed after
the value (optional)

During key navigation, pressing 'space' puts the control into 'edit'
mode, where the text can be directly edited, and up/down keys can be
used to increase/decrease the value. Pressing 'enter' or 'return' exits
edit mode.

This change also adds DecimalConverter to allow both
SwitchableOutputDelegate and NumberSelectorDialog to show spin boxes
with floating-point values.

Contributes to #2303

components/dialogs/NumberSelectorDialog.qml
@@ -21,10 +21,6 @@ ModalDialog {
 	signal maxValueReached()
 	signal minValueReached()
 
-	function _multiplier() {
-		return Math.pow(10, decimals)
-	}
-
 	onAboutToShow: {
 		if (presets.length) {
 			let presetsIndex = -1
@@ -50,8 +46,6 @@ ModalDialog {
 			SpinBox {
 				id: spinBox
 
-				property bool _initialized: false
-
 				anchors.horizontalCenter: parent.horizontalCenter
 				width: parent.width - 2*Theme.geometry_modalDialog_content_horizontalMargin
 				height: Theme.geometry_timeSelector_spinBox_height
@@ -59,21 +53,16 @@ ModalDialog {
 				indicatorImplicitWidth: root.decimals > 0
 						? Theme.geometry_spinBox_indicator_minimumWidth
 						: Theme.geometry_spinBox_indicator_maximumWidth
-				textFromValue: function(value, locale) {
-					return Units.formatNumber(value / root._multiplier(), root.decimals)
-				}
-				valueFromText: function(text, locale) {
-					let value = Units.formattedNumberToReal(text) * root._multiplier()
-					if (isNaN(value)) {
-						// don't change the current value
-						value = spinBox.value
-					}
-					return value
-				}
-				from: Math.max(Global.int32Min, root.from * root._multiplier())
-				to: Math.min(Global.int32Max, root.to * root._multiplier())
-				stepSize: root.stepSize * root._multiplier()
 				suffix: root.suffix
+				from: decimalConverter.intFrom
+				to: decimalConverter.intTo
+				stepSize: decimalConverter.intStepSize
+				value: decimalConverter.decimalToInt(root.value)
+				textFromValue: (value, locale) => decimalConverter.textFromValue(value)
+				valueFromText: (text, locale) => {
+					const v = decimalConverter.valueFromText(text)
+					return isNaN(v) ? decimalConverter.decimalToInt(root.value) : v // if invalid, use the previous value
+				}
 
 				// Use BeforeItem priority to override the default key Spinbox event handling, else
 				// up/down keys will modify the number even when SpinBox is not in "edit" mode.
@@ -82,18 +71,20 @@ ModalDialog {
 				KeyNavigation.up: spinBox
 				KeyNavigation.down: presetsRow.enabled ? presetsRow : root.footer
 
-				onValueChanged: {
-					if (_initialized) {
-						root.value = Number(spinBox.value / root._multiplier())
-						presetsRow.currentIndex = -1
-					}
+				onValueModified: {
+					root.value = decimalConverter.intToDecimal(spinBox.value)
+					presetsRow.currentIndex = -1
 				}
-
 				onMinValueReached: root.minValueReached()
 				onMaxValueReached: root.maxValueReached()
-				Component.onCompleted: {
-					spinBox.value = Math.round(root.value * root._multiplier())
-					_initialized = true
+
+				SpinBoxDecimalConverter {
+					id: decimalConverter
+
+					decimals: root.decimals
+					from: root.from
+					to: root.to
+					stepSize: root.stepSize
 				}
 			}
 
@@ -106,7 +97,7 @@ ModalDialog {
 				visible: model.length > 0
 				enabled: visible
 				onButtonClicked: function (buttonIndex) {
-					spinBox.value = model[buttonIndex].value * root._multiplier()
+					spinBox.value = decimalConverter.decimalToInt(model[buttonIndex].value)
 				}
 
 				KeyNavigation.down: root.footer

CMakeLists.txt
@@ -341,6 +341,7 @@ set (VENUS_QML_MODULE_SOURCES
     components/SolarYieldGraph.qml
     components/SolarYieldModel.qml
     components/SplashView.qml
+    components/SpinBoxDecimalConverter.qml
     components/StatusBar.qml
     components/SwipePageModel.qml
     components/SwipeViewPage.qml
@@ -375,6 +376,7 @@ set (VENUS_QML_MODULE_SOURCES
     components/controls/Label.qml
     components/controls/ListItemButton.qml
     components/controls/ListPressArea.qml
+    components/controls/MiniSpinBox.qml
     components/controls/MomentaryButton.qml
     components/controls/PressArea.qml
     components/controls/PressEffect.qml

components/SpinBoxDecimalConverter.qml
@@ -1,0 +1,36 @@
+/*
+** Copyright (C) 2025 Victron Energy B.V.
+** See LICENSE.txt for license information.
+*/
+
+import QtQuick
+import Victron.VenusOS
+
+QtObject {
+	required property int decimals
+	property real from
+	property real to
+	property real stepSize
+
+	readonly property int intFrom: Math.max(Global.int32Min, from * decimalFactor)
+	readonly property int intTo: Math.min(Global.int32Max, to * decimalFactor)
+	readonly property int intStepSize: stepSize * decimalFactor
+
+	readonly property int decimalFactor: Math.pow(10, decimals)
+
+	function decimalToInt(value) {
+		return value * decimalFactor
+	}
+
+	function intToDecimal(value) {
+		return value / decimalFactor
+	}
+
+	function textFromValue(value) {
+		return Units.formatNumber(value / decimalFactor, decimals)
+	}
+
+	function valueFromText(text) {
+		return Units.formattedNumberToReal(text) * decimalFactor
+	}
+}

components/SwitchableOutputDelegate.qml
@@ -4,6 +4,7 @@
 */
 import QtQuick
 import Victron.VenusOS
+import QtQuick.Templates as CT
 
 BaseListItem {
 	id: root
@@ -492,7 +493,71 @@ BaseListItem {
 	Component {
 		id: unrangedSetpointComponent
 
-		PlaceholderDelegate {}
+		MiniSpinBox {
+			id: spinBox
+
+			function handlePress(key) {
+				// Enter edit mode when space is pressed.
+				switch (key) {
+				case Qt.Key_Space:
+					focus = true
+					return true
+				default:
+					return false
+				}
+			}
+
+			width: root._buttonWidth
+			height: Theme.geometry_switchableoutput_button_height
+			enabled: !unrangedValueSync.busy
+			editable: !Global.isGxDevice // no room for VKB in the switch pane
+			suffix: unrangedUnit.value ?? ""
+			from: decimalConverter.intFrom
+			to: decimalConverter.intTo
+			stepSize: decimalConverter.intStepSize
+			value: decimalConverter.decimalToInt(unrangedValueSync.backendValue)
+			textFromValue: (value, locale) => decimalConverter.textFromValue(value)
+			valueFromText: (text, locale) => {
+				const v = decimalConverter.valueFromText(text)
+				return isNaN(v) ? decimalConverter.decimalToInt(unrangedValueSync.backendValue) : v
+			}
+			onValueModified: {
+				// Update the /Dimming value to the user-entered value.
+				unrangedValueSync.writeValue(decimalConverter.intToDecimal(value))
+			}
+
+			VeQuickItem {
+				id: unrangedMin
+				uid: root.outputUid + "/Settings/DimmingMin"
+			}
+			VeQuickItem {
+				id: unrangedMax
+				uid: root.outputUid + "/Settings/DimmingMax"
+			}
+			VeQuickItem {
+				id: unrangedStepSize
+				uid: root.outputUid + "/Settings/StepSize"
+			}
+			VeQuickItem {
+				id: unrangedUnit
+				uid: root.outputUid + "/Settings/Unit"
+			}
+
+			SpinBoxDecimalConverter {
+				id: decimalConverter
+
+				decimals: 2
+				from: unrangedMin.valid ? unrangedMin.value : 0
+				to: unrangedMax.valid ? unrangedMax.value : 100
+				stepSize: unrangedStepSize.valid ? unrangedStepSize.value : 1
+			}
+
+			SettingSync {
+				id: unrangedValueSync
+				backendValue: output.dimming
+				onUpdateToBackend: (value) => { output.setDimming(value) }
+			}
+		}
 	}
 
 	Component {

components/controls/EditFrame.qml
@@ -10,6 +10,7 @@ import Victron.VenusOS
 Rectangle {
 	id: root
 
+	property real arrowMargin: Theme.geometry_focus_highlight_border_size
 
 	color: "transparent"
 	border.color: Theme.color_focus_highlight
@@ -19,7 +20,7 @@ Rectangle {
 	CP.ColorImage {
 		anchors {
 			top: parent.top
-			topMargin: -height - Theme.geometry_focus_highlight_border_size
+			topMargin: -height - root.arrowMargin
 			horizontalCenter: parent.horizontalCenter
 		}
 		source: "qrc:/images/spinbox_arrow_up.svg"
@@ -29,7 +30,7 @@ Rectangle {
 	CP.ColorImage {
 		anchors {
 			bottom: parent.bottom
-			bottomMargin: -height - Theme.geometry_focus_highlight_border_size
+			bottomMargin: -height - root.arrowMargin
 			horizontalCenter: parent.horizontalCenter
 		}
 		source: "qrc:/images/spinbox_arrow_up.svg"

components/controls/MiniSpinBox.qml
@@ -1,0 +1,146 @@
+/*
+** Copyright (C) 2025 Victron Energy B.V.
+** See LICENSE.txt for license information.
+*/
+
+import QtQuick
+import QtQuick.Templates as CT
+import QtQuick.Controls.impl as CP
+import Victron.VenusOS
+
+/*
+	Provides a small SpinBox with an editable text input.
+
+	The up/down indicators can be clicked to increase/decrease the value.
+
+	When focused, a text cursor appears and the text can be edited directly. Press Enter/Return
+	to confirm the value, or Escape to revert to the previous value.
+
+	If focused while key key navigation is enabled, an orange frame with up/down arrow hints will
+	appear around the text input, and pressing Up/Down will increase/decrease the value.
+*/
+CT.SpinBox {
+	id: root
+
+	property string suffix
+	readonly property real indicatorWidth: width / 4
+
+	implicitWidth: Math.max(implicitBackgroundWidth + leftInset + rightInset,
+			implicitContentWidth + leftPadding + rightPadding)
+	implicitHeight: Math.max(implicitBackgroundHeight + topInset + bottomInset,
+			implicitContentHeight + topPadding + bottomPadding)
+	leftPadding: indicatorWidth
+	rightPadding: indicatorWidth
+	editable: true
+
+	onValueChanged: inputArea.setTextFromValue(value)
+	Component.onCompleted: inputArea.setTextFromValue(value)
+
+	contentItem: FocusScope {
+		// needed for QQuickSpinBoxPrivate to read the "text" property of the contentItem
+		// so that it can call the valueFromText() function
+		readonly property alias text: inputArea.text
+
+		Rectangle {
+			anchors {
+				fill: parent
+				// Push out the left/right edges so we don't get a double border when placed
+				// adjacent to the up/down indicators which also have borders.
+				leftMargin: -border.width
+				rightMargin: -border.width
+			}
+			color: root.enabled ? Theme.color_background_secondary : Theme.color_background_disabled
+			border.color: enabled ? Theme.color_ok : Theme.color_font_disabled
+			border.width: Theme.geometry_button_border_width
+		}
+
+		Keys.onPressed: (event) => {
+			switch (event.key) {
+			case Qt.Key_Return:
+			case Qt.Key_Enter:
+			case Qt.Key_Escape:
+				// Remove the focus from the entire SpinBox, so that up/down keys do not trigger
+				// the default increase/decrease behaviour (as that should only occur when the
+				// orange up/down hint frame is shown).
+				root.focus = false
+				event.accepted = true
+				break
+			case Qt.Key_Up:
+			case Qt.Key_Down:
+				// Accept the event to avoid triggering the default SpinBox increase/decrease
+				// behaviour.
+				event.accepted = true
+				break
+			default:
+				event.accepted = false
+				break
+			}
+		}
+
+		// The input area takes the initial focus when the SpinBox is focused during key navigation.
+		// When Enter/Return/Escape is pressed, focus is removed from the SpinBox.
+		SpinBoxInputArea {
+			id: inputArea
+
+			anchors.fill: parent
+			clip: true
+			spinBox: root
+			suffix: root.suffix
+			fontPixelSize: Theme.font_size_body2
+			arrowKeysEnabled: upDownHintFrame.visible
+			focus: Global.keyNavigationEnabled
+		}
+
+		// Orange frame with arrow indicators, to hint that up/down keys will change the value,
+		// when key navigation is enabled.
+		EditFrame {
+			id: upDownHintFrame
+			anchors.fill: parent
+			visible: Global.keyNavigationEnabled && root.activeFocus
+			arrowMargin: 0
+		}
+	}
+
+	down.indicator: Rectangle {
+		implicitWidth: root.indicatorWidth
+		implicitHeight: root.height
+		topLeftRadius: Theme.geometry_button_radius
+		bottomLeftRadius: Theme.geometry_button_radius
+		color: enabled
+			   ? (root.down.pressed ? Theme.color_ok : Theme.color_darkOk)
+			   : Theme.color_background_disabled
+		border.color: enabled ? Theme.color_ok : Theme.color_font_disabled
+		border.width: Theme.geometry_button_border_width
+
+		// Disable up/down indicators while text is edited directly, as indicators increase/decrease
+		// based on the actual SpinBox value, which may be different from the value shown by the
+		// text input if it has not yet been accepted.
+		enabled: !(root.editable && inputArea.activeFocus)
+
+		CP.ColorImage {
+			anchors.centerIn: parent
+			source: 'qrc:/images/icon_minus.svg'
+			color: enabled ? Theme.color_white : Theme.color_font_disabled
+		}
+	}
+
+	up.indicator: Rectangle {
+		x: parent.width - width
+		implicitWidth: root.indicatorWidth
+		implicitHeight: root.height
+		topRightRadius: Theme.geometry_button_radius
+		bottomRightRadius: Theme.geometry_button_radius
+		color: enabled
+			   ? (root.up.pressed ? Theme.color_ok : Theme.color_darkOk)
+			   : Theme.color_background_disabled
+		border.color: enabled ? Theme.color_ok : Theme.color_font_disabled
+		border.width: Theme.geometry_button_border_width
+		enabled: !(root.editable && inputArea.activeFocus)
+
+		CP.ColorImage {
+			anchors.centerIn: parent
+			source: 'qrc:/images/icon_plus.svg'
+			color: enabled ? Theme.color_white : Theme.color_font_disabled
+		}
+	}
+}

data/common/SwitchableOutput.qml
@@ -98,7 +98,9 @@ QtObject {
 	}
 
 	readonly property VeQuickItem _dimming: VeQuickItem {
-		uid: root.type === VenusOS.SwitchableOutput_Type_Dimmable ? `${root.uid}/Dimming` : ""
+		uid: root.type === VenusOS.SwitchableOutput_Type_Dimmable
+				|| root.type === VenusOS.SwitchableOutput_Type_UnrangedSetpoint
+			 ? `${root.uid}/Dimming` : ""
 	}
 
 	readonly property VeQuickItem _customName: VeQuickItem {

commit f6988741fd7efda1e0a9f540770a930e872ae820

Current limits: apply limit preset when selected
When a preset is selected in the current limit dialog, update the Dialog
value, and not just the SpinBox value.

Regression from 0a7e80ec6d6503fb343056eecb6b763f722ea953.

Fixes #2429

components/dialogs/NumberSelectorDialog.qml
@@ -35,6 +35,12 @@ ModalDialog {
 	}
 
 	contentItem: ModalDialog.FocusableContentItem {
+		id: dialogContent
+
+		function valueModified() {
+			root.value = decimalConverter.intToDecimal(spinBox.value)
+		}
+
 		Column {
 			anchors {
 				verticalCenter: parent.verticalCenter
@@ -72,7 +78,7 @@ ModalDialog {
 				KeyNavigation.down: presetsRow.enabled ? presetsRow : root.footer
 
 				onValueModified: {
-					root.value = decimalConverter.intToDecimal(spinBox.value)
+					dialogContent.valueModified()
 					presetsRow.currentIndex = -1
 				}
 				onMinValueReached: root.minValueReached()
@@ -98,6 +104,7 @@ ModalDialog {
 				enabled: visible
 				onButtonClicked: function (buttonIndex) {
 					spinBox.value = decimalConverter.decimalToInt(model[buttonIndex].value)
+					dialogContent.valueModified()
 				}
 
 				KeyNavigation.down: root.footer