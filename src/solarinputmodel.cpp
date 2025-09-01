f3a193d9b5056f7e85549d721166d6d1396a5e67
Bea Lam <bea.lam@qinetic.com.au>
2025-09-02 09:23:38 +1000

    Models: populate SolarInputModel internally in C++ model
    Populate SolarInputModel internally inside the model, instead of from a
    QML Instatiator in SolarInputListPage.qml. Using the Instantiator is
    slower as it needs to repeatedly update data from QML to the C++ model.
    This change also allows the sorting to be done in the C++ model.
    
    Also in BriefAndOverviewPageConfig, ensure multi services are also
    removed, else their solar devices will remain even when the config
    is not in use.
    
    Contributes to #2371

--- src/solarinputmodel.cpp
+++ src/solarinputmodel.cpp
@@ -4,155 +4,70 @@
 */
 
 #include "solarinputmodel.h"
+#include "solarinput.h"
+#include "alldevicesmodel.h"
+#include "device.h"
+
+#include <veutil/qt/ve_qitem.hpp>
+
+#include <QQmlInfo>
 
 using namespace Victron::VenusOS;
 
 SolarInputModel::SolarInputModel(QObject *parent)
 	: QAbstractListModel(parent)
 {
+	addAvailableInputs();
+
+	AllDevicesModel *allDevicesModel = AllDevicesModel::create();
+	connect(allDevicesModel, &AllDevicesModel::rowsInserted,
+			this, &SolarInputModel::sourceDeviceAdded);
+	connect(allDevicesModel, &AllDevicesModel::rowsAboutToBeRemoved,
+			this, &SolarInputModel::sourceDeviceAboutToBeRemoved);
+	connect(allDevicesModel, &AllDevicesModel::modelAboutToBeReset, [this]() {
+		beginResetModel();
+		clearInputs();
+	});
+	connect(allDevicesModel, &AllDevicesModel::modelAboutToBeReset, [this]() {
+		addAvailableInputs();
+		endResetModel();
+		emit countChanged();
+	});
 }
 
 int SolarInputModel::count() const
 {
-	return m_inputs.count();
-}
-
-int SolarInputModel::insertionIndex(const Input &input) const
-{
-	for (int i = 0; i < m_inputs.count(); ++i) {
-		if (m_inputs.at(i).group > input.group) {
-			return i;
-		} else if (m_inputs.at(i).group == input.group) {
-			if (m_inputs.at(i).name.localeAwareCompare(input.name) > 0) {
-				return i;
-			}
-		}
-	}
-	return m_inputs.count();
-}
-
-void SolarInputModel::addInput(const QString &serviceUid, const QVariantMap &values, int trackerIndex)
-{
-	const int removalIndex = indexOf(serviceUid, trackerIndex);
-	if (removalIndex >= 0) {
-		// The input is already in the list, so remove and and re-insert it, to preserve the sort
-		// order without re-sorting the entire list.
-		removeAt(removalIndex);
-	}
-
-	Input input = {
-		.serviceUid = serviceUid,
-		.group = values["group"].toString(),
-		.name = values["name"].toString(),
-		.todaysYield = values["todaysYield"].value<qreal>(),
-		.energy = values["energy"].value<qreal>(),
-		.power = values["power"].value<qreal>(),
-		.current = values["current"].value<qreal>(),
-		.voltage = values["voltage"].value<qreal>(),
-		.trackerIndex = trackerIndex,
-	};
-
-	// Add the input in a sorted order, based on the name and group.
-	const int index = insertionIndex(input);
-	beginInsertRows(QModelIndex(), index, index);
-	m_inputs.insert(index, input);
-	endInsertRows();
-	emit countChanged();
-}
-
-void SolarInputModel::setInputValue(const QString &serviceUid, Role role, const QVariant &value, int trackerIndex)
-{
-	const int index = indexOf(serviceUid, trackerIndex);
-	if (index < 0) {
-		return;
-	}
-
-	Input &input = m_inputs[index];
-	switch (role)
-	{
-	case ServiceUidRole:
-		qWarning() << "serviceUid is read-only";
-		break;
-	case GroupRole:
-		input.group = value.toString();
-		emit dataChanged(createIndex(index, 0), createIndex(index, 0), { GroupRole });
-		break;
-	case NameRole:
-		input.name = value.toString();
-		emit dataChanged(createIndex(index, 0), createIndex(index, 0), { NameRole });
-		break;
-	case TodaysYieldRole:
-		input.todaysYield = value.value<qreal>();
-		emit dataChanged(createIndex(index, 0), createIndex(index, 0), { TodaysYieldRole });
-		break;
-	case EnergyRole:
-		input.energy = value.value<qreal>();
-		emit dataChanged(createIndex(index, 0), createIndex(index, 0), { EnergyRole });
-		break;
-	case PowerRole:
-		input.power = value.value<qreal>();
-		emit dataChanged(createIndex(index, 0), createIndex(index, 0), { PowerRole });
-		break;
-	case CurrentRole:
-		input.current = value.value<qreal>();
-		emit dataChanged(createIndex(index, 0), createIndex(index, 0), { CurrentRole });
-		break;
-	case VoltageRole:
-		input.voltage = value.value<qreal>();
-		emit dataChanged(createIndex(index, 0), createIndex(index, 0), { VoltageRole });
-		break;
-	default:
-		break;
-	}
-}
-
-int SolarInputModel::indexOf(const QString &serviceUid, int trackerIndex) const
-{
-	for (int i = 0; i < m_inputs.count(); ++i) {
-		const Input &input = m_inputs.at(i);
-		if (input.serviceUid == serviceUid && input.trackerIndex == trackerIndex) {
-			return i;
-		}
-	}
-	return -1;
-}
-
-void SolarInputModel::removeAt(int index)
-{
-	if (index >= 0 && index < count()) {
-		beginRemoveRows(QModelIndex(), index, index);
-		m_inputs.removeAt(index);
-		endRemoveRows();
-		emit countChanged();
-	}
+	return m_enabledInputs.count();
 }
 
 QVariant SolarInputModel::data(const QModelIndex &index, int role) const
 {
 	const int row = index.row();
-	if (row < 0 || row >= m_inputs.count()) {
+	if (row < 0 || row >= m_enabledInputs.count()) {
 		return QVariant();
 	}
 
-	const Input &input = m_inputs.at(row);
+	const SolarInput *input = m_enabledInputs.at(row);
 	switch (role)
 	{
 	case ServiceUidRole:
-		return input.serviceUid;
+		return input->serviceUid();
+	case ServiceTypeRole:
+		return input->serviceType();
 	case GroupRole:
-		return input.group;
+		return input->group();
+	case EnabledRole:
+		return input->isEnabled();
 	case NameRole:
-		return input.name;
+		return input->name();
 	case TodaysYieldRole:
-		return input.todaysYield;
-	case EnergyRole:
-		return input.energy;
+		return input->todaysYield();
 	case PowerRole:
-		return input.power;
+		return input->power();
 	case CurrentRole:
-		return input.current;
+		return input->current();
 	case VoltageRole:
-		return input.voltage;
+		return input->voltage();
 	default:
 		return QVariant();
 	}
@@ -165,15 +80,234 @@ int SolarInputModel::rowCount(const QModelIndex &) const
 
 QHash<int, QByteArray> SolarInputModel::roleNames() const
 {
-	static QHash<int, QByteArray> roles = {
+	static const QHash<int, QByteArray> roles {
 		{ ServiceUidRole, "serviceUid" },
+		{ ServiceTypeRole, "serviceType" },
 		{ GroupRole, "group" },
+		{ EnabledRole, "enabled" },
 		{ NameRole, "name" },
 		{ TodaysYieldRole, "todaysYield" },
-		{ EnergyRole, "energy" },
 		{ PowerRole, "power" },
 		{ CurrentRole, "current" },
 		{ VoltageRole, "voltage" },
 	};
 	return roles;
+}
+
+void SolarInputModel::maybeAddDevice(Device *device)
+{
+	if (!device || !device->serviceItem()) {
+		return;
+	}
+
+	if (device->serviceType() == QStringLiteral("solarcharger")
+			|| device->serviceType() == QStringLiteral("multi")
+			|| device->serviceType() == QStringLiteral("inverter")) {
+		// For solarcharger/multi/inverter services, add their trackers (rather than the devices
+		// themselves) to the model. The number of trackers for each service is determined by the
+		// NrOfTrackers value.
+		QVariant trackerCountValue;
+		int trackerCount = 0;
+		if (VeQItem *nrOfTrackersItem = device->serviceItem()->itemGet(QStringLiteral("NrOfTrackers"))) {
+			trackerCountValue = nrOfTrackersItem->getValue();
+		}
+		if (trackerCountValue.isValid()) {
+			bool ok = false;
+			const int trackerCountInt = trackerCountValue.toInt(&ok);
+			if (ok) {
+				trackerCount = trackerCountInt;
+			}
+		} else if (device->serviceType() == QStringLiteral("solarcharger")) {
+			// For solarcharger services, there is always solar data even if /NrOfTrackers is not
+			// set; the first TrackerSolarInput will report the overall charger measurements.
+			trackerCount = 1;
+		}
+
+		// If a tracker is disabled, do not add it to the model, but monitor it in case it becomes
+		// enabled.
+		QList<int> enabledTrackerIndexes;
+		for (int i = 0; i < trackerCount; ++i) {
+			if (TrackerSolarInput::isEnabledTracker(device->serviceItem(), i)) {
+				enabledTrackerIndexes.append(i);
+			} else {
+				TrackerSolarInput *input = new TrackerSolarInput(device, trackerCount == 1, i, this);
+				initializeInput(input);
+				m_disabledInputs.append(input);
+			}
+		}
+
+		// Add all enabled trackers to the model.
+		if (enabledTrackerIndexes.count() > 0) {
+			beginInsertRows(QModelIndex(), m_enabledInputs.count(), m_enabledInputs.count());
+		}
+		for (int i = 0; i < enabledTrackerIndexes.count(); ++i) {
+			TrackerSolarInput *input = new TrackerSolarInput(device, trackerCount == 1, enabledTrackerIndexes.at(i), this);
+			initializeInput(input);
+			m_enabledInputs.append(input);
+		}
+		if (enabledTrackerIndexes.count() > 0) {
+			endInsertRows();
+			emit countChanged();
+		}
+	} else if (device->serviceType() == QStringLiteral("pvinverter")) {
+		// For pvinverter services, add each device to the model.
+		beginInsertRows(QModelIndex(), m_enabledInputs.count(), m_enabledInputs.count());
+		PvInverterSolarInput *input = new PvInverterSolarInput(device, this);
+		initializeInput(input);
+		m_enabledInputs.append(input);
+		endInsertRows();
+		emit countChanged();
+	}
+}
+
+void SolarInputModel::sourceDeviceAdded(const QModelIndex &parent, int first, int last)
+{
+	Q_UNUSED(parent)
+
+	for (int i = first; i <= last; ++i) {
+		maybeAddDevice(AllDevicesModel::create()->deviceAt(i));
+	}
+}
+
+void SolarInputModel::sourceDeviceAboutToBeRemoved(const QModelIndex &parent, int first, int last)
+{
+	Q_UNUSED(parent)
+
+	for (int i = first; i <= last; ++i) {
+		if (BaseDevice *device = AllDevicesModel::create()->deviceAt(i)) {
+			// Remove all enabled/disabled inputs that are connected to this device. If the device
+			// has multiple trackers, then all of them will be removed.
+			QList<int> enabledIndexesToRemove;
+			for (int i = 0; i < m_enabledInputs.count(); ++i) {
+				if (m_enabledInputs.at(i)->serviceUid() == device->serviceUid()) {
+					enabledIndexesToRemove.append(i);
+				}
+			}
+			for (int i = enabledIndexesToRemove.count() - 1; i >= 0; --i) {
+				beginRemoveRows(QModelIndex(), enabledIndexesToRemove.at(i), enabledIndexesToRemove.at(i));
+				SolarInput *input = m_enabledInputs.takeAt(enabledIndexesToRemove.at(i));
+				input->disconnect(this);
+				delete input;
+				endRemoveRows();
+				emit countChanged();
+			}
+			for (auto it = m_disabledInputs.begin(); it != m_disabledInputs.end();) {
+				if ((*it)->serviceUid() == device->serviceUid()) {
+					(*it)->disconnect(this);
+					delete *it;
+					it = m_disabledInputs.erase(it);
+				} else {
+					++it;
+				}
+			}
+		}
+	}
+}
+
+void SolarInputModel::clearInputs()
+{
+	qDeleteAll(m_enabledInputs);
+	m_enabledInputs.clear();
+	qDeleteAll(m_disabledInputs);
+	m_disabledInputs.clear();
+}
+
+void SolarInputModel::addAvailableInputs()
+{
+	AllDevicesModel *allDevicesModel = AllDevicesModel::create();
+	for (int i = 0; i < allDevicesModel->count(); ++i) {
+		maybeAddDevice(allDevicesModel->deviceAt(i));
+	}
+}
+
+void SolarInputModel::initializeInput(SolarInput *input)
+{
+	if (!input) {
+		qmlWarning(this) << "cannot initialize invalid input!";
+		return;
+	}
+	connect(input, &SolarInput::enabledChanged, this, &SolarInputModel::inputEnabledChanged);
+	connect(input, &SolarInput::nameChanged, this, [this, input]() { emitInputValueChanged(input, NameRole); });
+	connect(input, &SolarInput::todaysYieldChanged, this, [this, input]() { emitInputValueChanged(input, TodaysYieldRole); });
+	connect(input, &SolarInput::powerChanged, this, [this, input]() { emitInputValueChanged(input, PowerRole); });
+	connect(input, &SolarInput::currentChanged, this, [this, input]() { emitInputValueChanged(input, CurrentRole); });
+	connect(input, &SolarInput::voltageChanged, this, [this, input]() { emitInputValueChanged(input, VoltageRole); });
+}
+
+void SolarInputModel::emitInputValueChanged(SolarInput *input, Role role)
+{
+	for (int i = 0; i < m_enabledInputs.count(); ++i) {
+		if (m_enabledInputs.at(i) == input) {
+			emit dataChanged(createIndex(i, 0), createIndex(i, 0), { role });
+			break;
+		}
+	}
+}
+
+void SolarInputModel::inputEnabledChanged()
+{
+	if (TrackerSolarInput *input = qobject_cast<TrackerSolarInput *>(sender())) {
+		int enabledIndex = -1;
+		int disabledIndex = -1;
+		for (int i = 0; i < m_enabledInputs.count(); ++i) {
+			if (m_enabledInputs.at(i) == input) {
+				enabledIndex = i;
+				break;
+			}
+		}
+		for (int i = 0; i < m_disabledInputs.count(); ++i) {
+			if (m_disabledInputs.at(i) == input) {
+				disabledIndex = i;
+				break;
+			}
+		}
+
+		if (input->isEnabled()) {
+			// Input is now enabled, so move it from m_disabledInputs to m_enabledInputs.
+			if (disabledIndex >= 0) {
+				m_disabledInputs.removeAt(disabledIndex);
+			}
+			if (enabledIndex < 0) {
+				beginInsertRows(QModelIndex(), m_enabledInputs.count(), m_enabledInputs.count());
+				m_enabledInputs.append(input);
+				endInsertRows();
+				emit countChanged();
+			}
+		} else {
+			// Input is now disabled, so move it from m_enabledInputs to m_disabledInputs.
+			if (enabledIndex >= 0) {
+				beginRemoveRows(QModelIndex(), enabledIndex, enabledIndex);
+				m_enabledInputs.removeAt(enabledIndex);
+				endRemoveRows();
+				emit countChanged();
+			}
+			if (disabledIndex < 0) {
+				m_disabledInputs.append(input);
+			}
+		}
+	}
+}
+
+SortedSolarInputModel::SortedSolarInputModel(QObject *parent)
+	: QSortFilterProxyModel(parent)
+{
+	sort(0, Qt::AscendingOrder);
+}
+
+bool SortedSolarInputModel::lessThan(const QModelIndex &sourceLeft, const QModelIndex &sourceRight) const
+{
+	const QString leftGroup = sourceModel()->data(
+			sourceModel()->index(sourceLeft.row(), sourceLeft.column()), SolarInputModel::GroupRole).toString();
+	const QString rightGroup = sourceModel()->data(
+			sourceModel()->index(sourceRight.row(), sourceRight.column()), SolarInputModel::GroupRole).toString();
+
+	if (leftGroup == rightGroup) {
+		const QString leftName = sourceModel()->data(
+				sourceModel()->index(sourceLeft.row(), sourceLeft.column()), SolarInputModel::NameRole).toString();
+		const QString rightName = sourceModel()->data(
+				sourceModel()->index(sourceRight.row(), sourceRight.column()), SolarInputModel::NameRole).toString();
+		return leftName.localeAwareCompare(rightName) < 0;
+	} else {
+		return leftGroup.localeAwareCompare(rightGroup) < 0;
+	}
 }
