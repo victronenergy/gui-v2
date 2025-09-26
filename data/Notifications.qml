victronenergy/gui-v2

commit 7886c8c2809c8dea6f57cda4f94133e3bb8c5fe3

Notifications: show toast pop-up immediately on new notification
When a new notification is inserted into the model, show the toast
pop-up immediately, instead of waiting until a notification property has
changed.

Also, check that the notification is still active before showing the
toast pop-up. Otherwise, if the toast is acknowledged, and then the
notification becomes inactive, the toast is shown again due to the
property change.

Fixes #2444

data/Notifications.qml
@@ -16,7 +16,8 @@ QtObject {
 			+ Math.max(informations.activeCount, informations.unAcknowledgedCount)
 
 	readonly property NotificationsModel allNotificationsModel: NotificationsModel {
-		onNotificationUpdated: (notification) => toastedNotification.onNotificationUpdated(notification)
+		onNotificationInserted: (notification) => toastedNotification.queueNotification(notification)
+		onNotificationUpdated: (notification) => toastedNotification.queueNotification(notification)
 		onNotificationRemoved: (notification) => toastedNotification.onNotificationRemoved(notification)
 	}
 
@@ -35,7 +36,7 @@ QtObject {
 			onTriggered: toastedNotif.requestToastForNotification(toastedNotif.pendingNotification)
 		}
 
-		function onNotificationUpdated(notif: BaseNotification) {
+		function queueNotification(notif: BaseNotification) {
 			if (!toastedNotif.pendingNotification || notif.dateTime >= toastedNotif.pendingNotification.dateTime) {
 				toastedNotif.pendingNotification = notif;
 				pendingNotificationTimer.restart();
@@ -43,11 +44,10 @@ QtObject {
 		}
 
 		function requestToastForNotification(notif: BaseNotification) {
-
 			// the notification must be acknowledged: false at the point of being updated
 			// (this is because injected notifications' active is always false)
 			// for a toast to be considered for raising (and preempting existing ones)
-			if (!notif.acknowledged) {
+			if (!notif.acknowledged && notif.active) {
 				toastedNotif.checkAndRemoveExistingToast(notif)
 				if (!toastedNotif.toast) {
 					let createdToast = Global.notificationLayer?.showToastNotification(notif.type, "")

commit a7a723132c3df568cad2b53c0536bbfdb78d93fb

Notifications: ensure injected notifications are shown
Regression from 7886c8c. Injected notifications have active=false, so do
not avoid showing toast popups for inactive notifications.

Fixes #2516

data/Notifications.qml
@@ -47,7 +47,7 @@ QtObject {
 			// the notification must be acknowledged: false at the point of being updated
 			// (this is because injected notifications' active is always false)
 			// for a toast to be considered for raising (and preempting existing ones)
-			if (!notif.acknowledged && notif.active) {
+			if (!notif.acknowledged) {
 				toastedNotif.checkAndRemoveExistingToast(notif)
 				if (!toastedNotif.toast) {
 					let createdToast = Global.notificationLayer?.showToastNotification(notif.type, "")