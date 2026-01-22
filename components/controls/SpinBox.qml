victronenergy/gui-v2

commit 5b194dadd4ff3e3ffd6feb7525bf93d70f6ee757

SpinBox: move text input area into SpinBoxInputArea
This is in preparation for adding MiniSpinBox for the new switch pane
controls. MiniSpinBox will need an input area similar to the one in
SpinBox, but with different up/down indicators and without the need to
move focus between the indicators and the main content area.

Remove the SpinBox_FocusMode enum, as the focus mode can be set and
determined by the activeFocus of the relevant components in the SpinBox,
which greatly simplifies the focus handling there.

Also fix SpinBox to consider the indicator geometry in the padding
instead of the implicit width/height, to align with typical SpinBox
customizations.

Also in ModalDialog, ensure that when key navigation is disabled,
the user cannot navigate from the SpinBox to the dialog footer.

Contributes to #2303

components/controls/SpinBox.qml
@@ -12,18 +12,35 @@ import Victron.VenusOS
 // When the button is released, 'stepSize' reverts to its original value.
 // TODO - find a way to do this without exposing the changes to 'stepSize', as it may surprise developers when the value changes unexpectedly.
 
+/*
+	Provides a SpinBox with key navigation features and an editable text input.
+
+	The up/down indicators can be clicked to increase/decrease the value. Alternatively, to change
+	the value when key navigation is enabled:
+
+	- When the contentItem is focused with the key navigation highlight, press Space to enter "edit"
+	mode. An orange frame with arrow hints appears around the text input, and pressing Up/Down will
+	increase/decrease the value. If SpinBox editable=true, a text cursor is shown and the text can
+	be edited directly.
+	- To exit "edit" mode, press Enter/Return to confirm the value, or Escape (to revert to the
+	previous value.
+	- Press left/right to move the key navigation focus between the up/down indicators and the text
+	input content item.
+
+	Or, when key navigation is not enabled:
+
+	- If SpinBox editable=true, a blue frame appears around the text. If the text is clicked, a
+	text cursor is shown and the text can be edited text directly. To remove the cursor and focus,
+	press Enter/Return to confirm the value, or Escape to revert to the previous value.
+*/
 T.SpinBox {
 	id: root
 
-	property alias textInput: primaryTextInput
 	property alias secondaryText: secondaryLabel.text
 	property int indicatorImplicitWidth: Theme.geometry_spinBox_indicator_minimumWidth
 	property int orientation: Qt.Horizontal
-	property alias suffix: suffixLabel.text
-
-	property int focusMode: Global.keyNavigationEnabled
-			? VenusOS.SpinBox_FocusMode_Navigate
-			: VenusOS.SpinBox_FocusMode_NoAction
+	property string suffix
+	property int fontPixelSize: secondaryText.length > 0 ? Theme.font_size_h2 : Theme.font_size_h3
 
 	property int _scalingFactor: 1
 	property int _originalStepSize
@@ -31,70 +48,16 @@ T.SpinBox {
 	signal maxValueReached()
 	signal minValueReached()
 
-	function _handleContentItemKeyPress(event) {
-		// When key navigation is enabled, press space key to enter 'edit' mode. In edit mode:
-		// - an edit frame appears around the text, with up/down arrow indicators
-		// - up/down keys increases/decreases the value
-		// - left/right keys are disabled
-		// - enter/return/escape keys exit 'edit' mode (if text input is directly editable, the
-		//   text is also accepted)
-		//   (TODO: would be nice if Escape key reverted to previous value; currently it only does
-		//   this if the text input is directly editable)
-		switch (event.key) {
-		case Qt.Key_Space:
-			if (root.focusMode === VenusOS.SpinBox_FocusMode_Navigate) {
-				root.focusMode = VenusOS.SpinBox_FocusMode_Edit
-				if (root.editable) {
-					primaryTextInput.forceActiveFocus()
-				}
-				return true
-			}
-			break
-		case Qt.Key_Return:
-		case Qt.Key_Enter:
-		case Qt.Key_Escape:
-			if (root.focusMode === VenusOS.SpinBox_FocusMode_Edit) {
-				root.focusMode = VenusOS.SpinBox_FocusMode_Navigate
-				return true
-			}
-			break
-		case Qt.Key_Escape:
-			if (root.focusMode === VenusOS.SpinBox_FocusMode_Edit) {
-				root.focusMode = VenusOS.SpinBox_FocusMode_Navigate
-				return true
-			}
-			break
-		case Qt.Key_Up:
-		case Qt.Key_Down:
-			if (root.focusMode === VenusOS.SpinBox_FocusMode_Edit) {
-				if (event.key === Qt.Key_Up) {
-					root.increase()
-				} else {
-					root.decrease()
-				}
-				return true
-			}
-			break
-		case Qt.Key_Left:
-		case Qt.Key_Right:
-			if (root.focusMode === VenusOS.SpinBox_FocusMode_Edit) {
-				return true
-			}
-			break
-		}
-		return false
-	}
-
 	implicitWidth: Math.max(implicitBackgroundWidth + leftInset + rightInset,
-		orientation === Qt.Horizontal
-			? valueColumn.width + up.indicator.width + down.indicator.width + (2 * spacing) + leftPadding + rightPadding
-			: valueColumn.width + leftPadding + rightPadding)
+			implicitContentWidth + leftPadding + rightPadding)
 	implicitHeight: Math.max(implicitBackgroundHeight + topInset + bottomInset,
-		orientation === Qt.Horizontal
-			? Math.max(valueColumn.height, up.indicator.height, down.indicator.height) + topPadding + bottomPadding
-			: valueColumn.height + up.indicator.height + down.indicator.height + (2 * spacing) + topPadding + bottomPadding)
-
+			implicitContentHeight + topPadding + bottomPadding)
+	leftPadding: orientation === Qt.Horizontal ? up.indicator.width + spacing : 0
+	rightPadding: orientation === Qt.Horizontal ? down.indicator.width + spacing : 0
+	topPadding: orientation === Qt.Vertical ? up.indicator.height + spacing : 0
+	bottomPadding: orientation === Qt.Vertical ? down.indicator.height + spacing : 0
 	spacing: Theme.geometry_spinBox_spacing
+
 	onValueModified: {
 		if (value === to) {
 			root.maxValueReached()
@@ -103,176 +66,142 @@ T.SpinBox {
 		}
 	}
 
-	onValueChanged: primaryTextInput.updateText()
+	// Update the displayed text when the initial value is set or when the up/down buttons are
+	// pressed.
+	onValueChanged: inputArea.setTextFromValue(value)
+	Component.onCompleted: inputArea.setTextFromValue(value)
 
-	Component.onCompleted: primaryTextInput.updateText()
-
-	contentItem: Item {
+	contentItem: FocusScope {
 		id: spinBoxContentItem
 
 		// needed for QQuickSpinBoxPrivate to read the "text" property of the contentItem
 		// so that it can call the valueFromText() function
-		readonly property alias text: primaryTextInput.text
+		readonly property alias text: inputArea.text
 
 		focus: Global.keyNavigationEnabled
 
-		Keys.onPressed: (event) => event.accepted = root._handleContentItemKeyPress(event)
-		Keys.enabled: Global.keyNavigationEnabled
-		KeyNavigation.left: root.down.indicator.enabled
-					&& root.focusMode === VenusOS.SpinBox_FocusMode_Navigate
-					&& root.orientation === Qt.Horizontal
+		KeyNavigation.left: Global.keyNavigationEnabled && root.down.indicator.enabled && root.orientation === Qt.Horizontal
 				? root.down.indicator
 				: null
-		KeyNavigation.right: root.up.indicator.enabled
-					&& root.focusMode === VenusOS.SpinBox_FocusMode_Navigate
-					&& root.orientation === Qt.Horizontal
+		KeyNavigation.right: Global.keyNavigationEnabled && root.up.indicator.enabled && root.orientation === Qt.Horizontal
 				? root.up.indicator
 				: null
-		KeyNavigation.up: root.up.indicator.enabled
-					&& root.focusMode === VenusOS.SpinBox_FocusMode_Navigate
-					&& root.orientation === Qt.Vertical
-				? root.up.indicator
-				: null
-		KeyNavigation.down: root.down.indicator.enabled
-					&& root.focusMode === VenusOS.SpinBox_FocusMode_Navigate
-					&& root.orientation === Qt.Vertical
+		KeyNavigation.up: Global.keyNavigationEnabled && root.down.indicator.enabled && root.orientation === Qt.Vertical
 				? root.down.indicator
 				: null
+		KeyNavigation.down: Global.keyNavigationEnabled && root.up.indicator.enabled && root.orientation === Qt.Vertical
+				? root.up.indicator
+				: null
 
-		// we include the additional GlobalKeyNavigationHighlight's visible binding conditions here
-		// because we want to know its absolute visibility, not just its "intention" to be active.
-		readonly property bool keyNavigationHighlightVisible: root.focusMode === VenusOS.SpinBox_FocusMode_Navigate
-															  && spinBoxContentItem.visible
-															  && spinBoxContentItem.activeFocus
-															  && Global.keyNavigationEnabled
-															  && !Global.pageManager?.expandLayout
+		// Called when a key is pressed while contentItem is focused, or when a key is pressed
+		// within the inner inputArea but not accepted there.
+		// The contentItem may be focused:
+		// - during key navigation, if it is explicitly selected by the user
+		// - indirectly, when the inner text input is clickable (as indicated by blue frame) and the
+		//   user clicks it to give it active focus
+		Keys.onPressed: (event) => {
+			switch (event.key) {
+			case Qt.Key_Space:
+				// When using key navigation, the Space key enters "edit" mode, where the
+				// upDownHintFrame is shown, and the text input area receives all key events.
+				if (Global.keyNavigationEnabled
+						// Don't activate if holding a press on the up/down buttons leads to this
+						// being focused.
+						&& !event.isAutoRepeat) {
+					inputArea.focus = true
+					event.accepted = true
+					return
+				}
+				break
+			case Qt.Key_Return:
+			case Qt.Key_Enter:
+			case Qt.Key_Escape:
+				if (upDownHintFrame.visible) {
+					// The user entered "edit" mode by pressing Space. Now move the focus back to
+					// the contentItem so key navigation can be used to navigate to the indicators.
+					inputArea.focus = false
+				} else if (clickableHintFrame.visible) {
+					// The user entered "edit" mode by clicking within the blue frame.
+					// Remove the focus from the entire SpinBox, so that up/down keys do not trigger
+					// the default increase/decrease behaviour (as that should only occur when the
+					// orange up/down hint frame is shown).
+					root.focus = false
+				}
+				event.accepted = true
+				return
+			case Qt.Key_Up:
+			case Qt.Key_Down:
+				if (clickableHintFrame.visible) {
+					// Accept the event to prevent focus from moving elsewhere.
+					event.accepted = true
+					return
+				} else {
+					// Allow key navigation to move the focus elsewhere.
+				}
+				break
+			default:
+				break
+			}
+			event.accepted = false
+		}
 
-		// Shows a highlight box around the text when key navigation is enabled and the SpinBox is
-		// in navigation mode.
-		KeyNavigationHighlight.active: keyNavigationHighlightVisible
-		KeyNavigationHighlight.fill: editHighlightArea
+		KeyNavigationHighlight.active: Global.keyNavigationEnabled && activeFocus
+		KeyNavigationHighlight.fill: contentArea
 
-		Column {
-			id: valueColumn
+		// This acts like a Column but cannot be one, as KeyNavigationHighlight.fill injects the
+		// highlight as a child with anchor bindings, which cannot be used in Row/Column.
+		Item {
+			id: contentArea
 
-			width: Math.max(primaryTextInputItem.implicitWidth, secondaryLabel.implicitWidth)
 			anchors.centerIn: parent
+			implicitWidth: Math.min(root.availableWidth, Math.max(inputArea.implicitWidth, secondaryLabel.implicitWidth))
+			height: secondaryLabel.y + secondaryLabel.height
 
-			Item  {
-				id: primaryTextInputItem
+			SpinBoxInputArea {
+				id: inputArea
 
-				width: primaryRowLayout.implicitWidth + Theme.geometry_textField_horizontalMargin * 2
-				height: primaryRowLayout.height
-				anchors.horizontalCenter: parent.horizontalCenter
-
-				MouseArea {
-					anchors.fill: parent
-					enabled: root.editable
-					onClicked: primaryTextInput.forceActiveFocus()
-				}
-
-				RowLayout {
-					id: primaryRowLayout
-
-					anchors.centerIn: parent
-
-					TextInput {
-						id: primaryTextInput
-
-						color: root.enabled ? Theme.color_font_primary : Theme.color_background_disabled
-						font.family: Global.fontFamily
-						font.pixelSize: root.secondaryText.length > 0 ? Theme.font_size_h2 : Theme.font_size_h3
-						horizontalAlignment: Qt.AlignHCenter
-						verticalAlignment: Qt.AlignVCenter
-						selectedTextColor: Theme.color_white
-						selectionColor : Theme.color_blue
-						readOnly: !root.editable
-						selectByMouse: !readOnly
-						validator: root.validator
-						inputMethodHints: root.inputMethodHints
-
-						onActiveFocusChanged: {
-							if (activeFocus && Global.keyNavigationEnabled) {
-								root.focusMode = VenusOS.SpinBox_FocusMode_Edit
-							}
-						}
-
-						onAccepted: {
-							// Note that the text may at this time represent a value out of SpinBox
-							// to/from range, so clamp it here.
-							let v = root.valueFromText(text, root.locale)
-							if (v < root.from) {
-								v = root.from
-							} else if (v > root.to) {
-								v = root.to
-							}
-
-							// Force-update the displayed text, to guarantee the text is in sync
-							// with the numeric value, even if the value has not changed due to the
-							// user entering an out-of-range value on consecutive attempts.
-							text = root.textFromValue(v, root.locale)
-							root.value = v
-
-							primaryTextInput.focus = false
-						}
-
-						Keys.onEscapePressed: (event) => {
-							// Restore the previous value and clear the focus.
-							text = root.textFromValue(root.value, root.locale)
-							primaryTextInput.focus = false
-							if (Global.keyNavigationEnabled) {
-								root.focusMode = VenusOS.SpinBox_FocusMode_Navigate
-							}
-							event.accepted = true
-						}
-
-						function updateText() {
-							// Update the displayed text when the initial value is set or when
-							// the up/down buttons are pressed.
-							primaryTextInput.text = root.textFromValue(root.value, root.locale)
-						}
-					}
-
-					Label {
-						id: suffixLabel
-
-						visible: text.length
-						color: primaryTextInput.color
-						font: primaryTextInput.font
-						horizontalAlignment: primaryTextInput.horizontalAlignment
-						verticalAlignment: primaryTextInput.verticalAlignment
-					}
-				}
+				width: contentArea.width
+				clip: true
+				spinBox: root
+				suffix: root.suffix
+				fontPixelSize: root.fontPixelSize
+				arrowKeysEnabled: upDownHintFrame.visible
+				focus: false
 			}
 
 			Label {
 				id: secondaryLabel
 
+				anchors {
+					top: inputArea.bottom
+					horizontalCenter: parent.horizontalCenter
+				}
 				height: text.length ? implicitHeight : 0
 				color: Theme.color_font_secondary
 				font.pixelSize: Theme.font_size_caption
 				horizontalAlignment: Qt.AlignHCenter
 			}
-		}
 
-		Item {
-			id: editHighlightArea
-
-			anchors.centerIn: parent
-			width: primaryTextInputItem.width
-			height: valueColumn.height
-
-			// Shows a box around the text when the SpinBox text can be edited directly (to indicate it
-			// can be clicked) or when key navigation is enabled and the SpinBox is in edit mode (to
-			// show up/down arrows indicating that the arrow keys can be used to change the value).
-			EditFrame {
+			// Blue rectangle indicateing the text area is clickable for direct text editing, when
+			// editable=true. Not shown when key navigation highlight or orange frame are shown.
+			Rectangle {
+				id: clickableHintFrame
 				anchors.fill: parent
-				visible: !spinBoxContentItem.keyNavigationHighlightVisible
-						 && (root.focusMode === VenusOS.SpinBox_FocusMode_Edit || root.editable)
-				border.color: root.focusMode === VenusOS.SpinBox_FocusMode_Edit
-							  ? Theme.color_focus_highlight
-							  : Theme.color_blue
-				arrowHintsVisible: root.focusMode === VenusOS.SpinBox_FocusMode_Edit
+				visible: root.editable
+						 && !upDownHintFrame.visible
+						 && !spinBoxContentItem.KeyNavigationHighlight.active
+				color: "transparent"
+				border.color: Theme.color_blue
+				border.width: Theme.geometry_focus_highlight_border_size
+				radius: Theme.geometry_button_radius
+			}
+
+			// Orange frame with arrow indicators, to hint that up/down keys will change the value,
+			// when key navigation is enabled.
+			EditFrame {
+				id: upDownHintFrame
+				anchors.fill: parent
+				visible: Global.keyNavigationEnabled && inputArea.activeFocus
 			}
 		}
 	}
@@ -283,7 +212,7 @@ T.SpinBox {
 		   : contentItem.x + (contentItem.width / 2) - (width / 2)
 		y: orientation === Qt.Horizontal
 		   ? contentItem.y + (contentItem.height / 2) - (height / 2)
-		   : contentItem.y - root.spacing
+		   : contentItem.y + (height / 2) + spacing
 		implicitWidth: root.indicatorImplicitWidth
 		implicitHeight: Theme.geometry_spinBox_indicator_height
 		radius: Theme.geometry_spinBox_indicator_radius
@@ -291,10 +220,16 @@ T.SpinBox {
 			   ? (root.up.pressed ? Theme.color_button_down : Theme.color_button)
 			   : Theme.color_background_disabled
 
+		// Disable up/down indicators while text is edited directly, as indicators increase/decrease
+		// based on the actual SpinBox value, which may be different from the value shown by the
+		// text input if it has not yet been accepted.
+		enabled: !inputArea.activeFocus && root.value < root.to
+
 		KeyNavigation.left: orientation === Qt.Horizontal ? root.contentItem : null
 		KeyNavigation.down: orientation === Qt.Vertical ? root.contentItem : null
 		Keys.onSpacePressed: {
 			root.increase()
+			root.valueModified()
 			if (!enabled) {
 				// Ensure focus is not in limbo if the button becomes disabled
 				root.contentItem.focus = true
@@ -317,18 +252,20 @@ T.SpinBox {
 		   : contentItem.x + (contentItem.width / 2) - (width / 2)
 		y: orientation === Qt.Horizontal
 		   ? contentItem.y + (contentItem.height / 2) - (height / 2)
-		   : contentItem.y + root.spacing + contentItem.height - height
+		   : -(height / 2)
 		implicitWidth: root.indicatorImplicitWidth
 		implicitHeight: Theme.geometry_spinBox_indicator_height
 		radius: Theme.geometry_spinBox_indicator_radius
 		color: enabled
 			   ? (root.down.pressed ? Theme.color_button_down : Theme.color_button)
 			   : Theme.color_background_disabled
+		enabled: !inputArea.activeFocus && root.value > root.from
 
 		KeyNavigation.right: orientation === Qt.Horizontal ? root.contentItem : null
 		KeyNavigation.up: orientation === Qt.Vertical ? root.contentItem : null
 		Keys.onSpacePressed: {
 			root.decrease()
+			root.valueModified()
 			if (!enabled) {
 				// Ensure focus is not in limbo if the button becomes disabled
 				root.contentItem.focus = true

CMakeLists.txt
@@ -385,6 +385,7 @@ set (VENUS_QML_MODULE_SOURCES
     components/controls/ScrollBar.qml
     components/controls/Slider.qml
     components/controls/SpinBox.qml
+    components/controls/SpinBoxInputArea.qml
     components/controls/SwipeView.qml
     components/controls/Switch.qml
     components/controls/TextField.qml

components/DateSelector.qml
@@ -28,7 +28,7 @@ FocusScope {
 			width: root.availableWidth > 0 ? (root.availableWidth - (Theme.geometry_modalDialog_content_horizontalMargin * 2)) / 3 : implicitWidth
 			orientation: Qt.Vertical
 			spacing: Theme.geometry_spinBox_wide_spacing
-			textInput.font.pixelSize: Theme.font_size_h2
+			fontPixelSize: Theme.font_size_h2
 			from: 1970
 			to: 2100
 
@@ -47,7 +47,7 @@ FocusScope {
 			width: root.availableWidth > 0 ? (root.availableWidth - (Theme.geometry_modalDialog_content_horizontalMargin * 2)) / 3 : implicitWidth
 			orientation: Qt.Vertical
 			spacing: Theme.geometry_spinBox_wide_spacing
-			textInput.font.pixelSize: Theme.font_size_h2
+			fontPixelSize: Theme.font_size_h2
 			from: 1
 			to: 12
 			textFromValue: (value, locale) => Utils.pad(value, 2)
@@ -64,7 +64,7 @@ FocusScope {
 			width: root.availableWidth > 0 ? (root.availableWidth - (Theme.geometry_modalDialog_content_horizontalMargin * 2)) / 3 : implicitWidth
 			orientation: Qt.Vertical
 			spacing: Theme.geometry_spinBox_wide_spacing
-			textInput.font.pixelSize: Theme.font_size_h2
+			fontPixelSize: Theme.font_size_h2
 			from: 1
 			to: root.year,root.month, ClockTime.daysInMonth(root.month, root.year)
 			textFromValue: (value, locale) => Utils.pad(value, 2)

components/controls/EditFrame.qml
@@ -10,7 +10,6 @@ import Victron.VenusOS
 Rectangle {
 	id: root
 
-	property bool arrowHintsVisible
 
 	color: "transparent"
 	border.color: Theme.color_focus_highlight
@@ -25,7 +24,6 @@ Rectangle {
 		}
 		source: "qrc:/images/spinbox_arrow_up.svg"
 		color: Theme.color_focus_highlight
-		visible: root.arrowHintsVisible
 	}
 
 	CP.ColorImage {
@@ -37,6 +35,5 @@ Rectangle {
 		source: "qrc:/images/spinbox_arrow_up.svg"
 		color: Theme.color_focus_highlight
 		rotation: 180
-		visible: root.arrowHintsVisible
 	}
 }

components/controls/SpinBoxInputArea.qml
@@ -1,0 +1,119 @@
+/*
+** Copyright (C) 2025 Victron Energy B.V.
+** See LICENSE.txt for license information.
+*/
+
+import QtQuick
+import QtQuick.Templates as T
+import Victron.VenusOS
+
+TextInput {
+	id: root
+
+	required property T.SpinBox spinBox
+	property alias suffix: suffixLabel.text
+	property int fontPixelSize: Theme.font_size_h3
+	property int initialValue
+	property bool arrowKeysEnabled
+
+	function setTextFromValue(value) {
+		text = spinBox.textFromValue(value, spinBox.locale)
+	}
+
+	// These change the displayed value (which may not yet be accepted), rather than the actual
+	// spinBox value.
+	function increase() {
+		setTextFromValue(Math.min(spinBox.to, spinBox.valueFromText(text, spinBox.locale) + spinBox.stepSize))
+	}
+	function decrease() {
+		setTextFromValue(Math.max(spinBox.from, spinBox.valueFromText(text, spinBox.locale) - spinBox.stepSize))
+	}
+
+	onActiveFocusChanged: {
+		if (activeFocus) {
+			root.initialValue = root.spinBox.value
+		}
+	}
+
+	Keys.onPressed: (event) => {
+		switch (event.key) {
+		case Qt.Key_Return:
+		case Qt.Key_Enter:
+			// Save the entered text. The text may at this time represent a value outside of the
+			// to/from range, so clamp it here.
+			let v = root.spinBox.valueFromText(text, root.spinBox.locale)
+			if (v < root.spinBox.from) {
+				v = root.spinBox.from
+			} else if (v > root.spinBox.to) {
+				v = root.spinBox.to
+			}
+
+			// Force-update the displayed text, to guarantee the text is in sync
+			// with the numeric value, even if the value has not changed due to the
+			// user entering an out-of-range value on consecutive attempts.
+			text = root.spinBox.textFromValue(v, root.spinBox.locale)
+			if (v !== spinBox.value) {
+				root.spinBox.value = v
+				root.spinBox.valueModified()
+			}
+			break
+		case Qt.Key_Escape:
+			// Restore the initial value.
+			setTextFromValue(root.initialValue)
+			break
+		case Qt.Key_Up:
+		case Qt.Key_Down:
+			if (arrowKeysEnabled) {
+				// Change the displayed input value (which may not yet be accepted) rather than
+				// calling SpinBox increase()/decrease(), which change the actual SpinBox value.
+				if (event.key === Qt.Key_Up) {
+					increase()
+				} else {
+					decrease()
+				}
+				event.accepted = true
+				return
+			}
+			break
+		case Qt.Key_Left:
+			event.accepted = cursorPosition <= 0
+			return
+		case Qt.Key_Right:
+			// Don't allow left/right keys to move focus away from the text input.
+			event.accepted = cursorPosition >= length
+			return
+		}
+		event.accepted = false
+	}
+
+	leftPadding: Theme.geometry_textField_horizontalMargin
+	rightPadding: suffixLabel.width + Theme.geometry_quantityLabel_spacing + Theme.geometry_textField_horizontalMargin
+	focus: root.spinBox.editable
+	color: enabled ? Theme.color_font_primary : Theme.color_font_disabled
+	font.family: Global.fontFamily
+	font.pixelSize: root.fontPixelSize
+	horizontalAlignment: Qt.AlignHCenter
+	verticalAlignment: Qt.AlignVCenter
+	selectedTextColor: Theme.color_white
+	selectionColor : Theme.color_blue
+	readOnly: !root.spinBox.editable
+	activeFocusOnPress: !readOnly && root.spinBox.editable
+	selectByMouse: activeFocusOnPress
+	validator: root.spinBox.validator
+	inputMethodHints: root.spinBox.inputMethodHints
+
+	Label {
+		id: suffixLabel
+
+		anchors {
+			centerIn: parent
+			horizontalCenterOffset: (parent.contentWidth / 2) + Theme.geometry_quantityLabel_spacing
+		}
+		width: text.length ? implicitWidth : 0
+		visible: text.length
+		color: parent.color
+		font: parent.font
+		horizontalAlignment: parent.horizontalAlignment
+		verticalAlignment: parent.verticalAlignment
+	}
+}

components/dialogs/ModalDialog.qml
@@ -61,7 +61,7 @@ T.Dialog {
 		// Give the initial focus to this content item so that child UI controls will receive focus.
 		focus: true
 
-		KeyNavigation.down: root.footer
+		KeyNavigation.down: Global.keyNavigationEnabled ? root.footer : null
 		Keys.onReturnPressed: root.handleAccept()
 		Keys.onEnterPressed: root.handleAccept()
 		Keys.onEscapePressed: root.handleReject()

components/listitems/core/SliderHandleHighlight.qml
@@ -16,5 +16,4 @@ EditFrame {
 	height: handle.height
 	y: -Theme.geometry_switch_indicator_shadowOffset
 	rotation: 90
-	arrowHintsVisible: true
 }

src/enums.h
@@ -60,13 +60,6 @@ public:
 	};
 	Q_ENUM(StatusBar_RightButton)
 
-	enum SpinBox_FocusMode {
-		SpinBox_FocusMode_NoAction,
-		SpinBox_FocusMode_Navigate,
-		SpinBox_FocusMode_Edit
-	};
-	Q_ENUM(SpinBox_FocusMode)
-
 	enum Units_Type {
 		Units_None = 0,
 		Units_Volume_CubicMetre,

commit dfb3e2a99b72ed0288258103e5e3ea0112c46b2f

UI Controls: fix date selector dialog layout in vertical orientation
Regression from 5b194dadd4ff3e3ffd6feb7525bf93d70f6ee757.

components/controls/SpinBox.qml
@@ -218,7 +218,7 @@ T.SpinBox {
 		   : contentItem.x + (contentItem.width / 2) - (width / 2)
 		y: orientation === Qt.Horizontal
 		   ? contentItem.y + (contentItem.height / 2) - (height / 2)
-		   : contentItem.y + (height / 2) + spacing
+		   : contentItem.y + contentItem.height + root.spacing
 		implicitWidth: root.indicatorImplicitWidth
 		implicitHeight: Theme.geometry_spinBox_indicator_height
 		radius: Theme.geometry_spinBox_indicator_radius
@@ -244,7 +244,7 @@ T.SpinBox {
 		   : contentItem.x + (contentItem.width / 2) - (width / 2)
 		y: orientation === Qt.Horizontal
 		   ? contentItem.y + (contentItem.height / 2) - (height / 2)
-		   : -(height / 2)
+		   : contentItem.y - root.spacing - height
 		implicitWidth: root.indicatorImplicitWidth
 		implicitHeight: Theme.geometry_spinBox_indicator_height
 		radius: Theme.geometry_spinBox_indicator_radius