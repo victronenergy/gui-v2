victronenergy/gui-v2

commit 7a73f73e6135978c3105486206e6a923d99eef67

Settings: remove top-right button customisations
Some settings pages customise the top-right button in the status bar.
This can be difficult for the user to see and trigger the appropriate
action, so remove these customisations and just show a normal list item
that triggers the same action.

This affects the following pages:
- Settings > Devices > [tank] > Setup > Custom shape
- Settings > Integrations > Modbus devices > Saved devices
- Settings > Integrations > PV inverters > Add IP address manually
- Settings > Integrations > PV inverters > Modbus port and unit ID
settings

This also requires fixes in KeyNavigationListHelper to ensure a view is
only focused if it has focusable children (i.e. children that return
true for Utils.acceptsKeyNavigation()). Otherwise, for example, in the
PV inverters "Add IP address manually" page, if the "Add IP address"
list item is hidden (i.e. should not be focusable), its SettingsColumn
parent can still be focused, even though it only has one child and that
child is not focusable.

Prevent that from happening, by adding _focusContainerOrItem() to return
the result of the focus attempt on the view. Also, set _currentIndex to
-1 when a header or footer is focused, otherwise the view will retain
the last focused index even if none of its delegates are focused, and
attempt to focus that index again.

Fixes #2646

components/KeyNavigationListHelper.qml
@@ -41,22 +41,25 @@ QtObject {
 
 		// If no items have been focused yet, or the previously focused item can no longer be
 		// focused, then find a new item to focus.
-		if (!currentItem || !Utils.acceptsKeyNavigation(currentItem)) {
+		if (!currentItem || !_focusContainerOrItem(currentItem)) {
 			if (_initialized && validCurrentIndex) {
 				// The current item is no longer focusable, so find a focusable item, working
 				// backwards from the current index.
 				if (focusPreviousItem(currentIndex)) {
-					return
+					return true
 				}
 			}
 			// Focus the first available item.
 			_initialized = true
-			if (Utils.acceptsKeyNavigation(headerItem)) {
+			if (_focusContainerOrItem(headerItem)) {
 				headerItem.focus = true
+				_currentIndex = -1
+				return true
 			} else {
-				focusNextItem(-1)
+				return focusNextItem(-1)
 			}
 		}
+		return _currentIndex >= 0 && _currentIndex < itemCount
 	}
 
 	// Moves the focus to the next item in the list.
@@ -67,6 +70,25 @@ QtObject {
 	// Moves the focus to the previous item in the list.
 	function focusPreviousItem(fromIndex) {
 		return _moveFocus(fromIndex, false)
+	}
+
+	// Moves focus to a view or item. If a view with a KeyNavigationListHelper is specified, returns
+	// the result of calling updateFocusedItem() on that view, so that the caller can determine
+	// whether the focus was moved successfully - e.g. it will fail if the view currently has no
+	// focusable items, and in that case the caller can try a different item.
+	// If a non-view is specified, this simply sets focus=true on the item.
+	function _focusContainerOrItem(containerOrItem) {
+		if (!Utils.acceptsKeyNavigation(containerOrItem)) {
+			return false
+		}
+		if (containerOrItem.__keyNavHelper) {
+			// Try to focus the first item in the container.
+			return containerOrItem.__keyNavHelper.updateFocusedItem()
+		} else {
+			// Focus the item.
+			containerOrItem.focus = true
+			return true
+		}
 	}
 
 	// Focuses the next item (fromIndex + 1) or previous item (fromIndex - 1).
@@ -98,27 +120,22 @@ QtObject {
 				// There are no more created delegates, so stop searching.
 				break
 			}
-			if (Utils.acceptsKeyNavigation(toItem)) {
-				_currentIndex = toIndex
 
-				// If moving away from a header/footer that is focused, remove the focus.
-				if (forward && headerItem?.activeFocus) {
-					headerItem.focus = false
-				} else if (!forward && footerItem?.activeFocus) {
-					footerItem.focus = false
-				}
-				toItem.focus = true
+			if (_focusContainerOrItem(toItem)) {
+				_currentIndex = toIndex
 				return true
 			}
 		}
 
 		// Allow key navigation to move to the footer (if moving forwards) or footer (if backwards).
-		if (forward && Utils.acceptsKeyNavigation(footerItem)) {
+		if (forward && _focusContainerOrItem(footerItem)) {
 			footerItem.focus = true
+			_currentIndex = -1
 			scrollToEndRequested() // ensure footer can be seen
 			return true
-		} else if (!forward && Utils.acceptsKeyNavigation(headerItem)) {
+		} else if (!forward && _focusContainerOrItem(headerItem)) {
 			headerItem.focus = true
+			_currentIndex = -1
 			scrollToStartRequested() // ensure header can be seen
 			return true
 		}

cmake/ModuleVenus_Sources.cmake
@@ -804,6 +804,7 @@ set(VictronVenusOS_RESOURCES
     images/icon_manualstart_24.svg
     images/icon_manualstart_timer_24.svg
     images/icon_minus.svg
+    images/icon_minus_32.svg
     images/icon_minus_bordered.svg
     images/icon_plus.svg
     images/icon_plus_32.svg

components/BaseListView.qml
@@ -20,6 +20,8 @@ import Victron.VenusOS
 ListView {
 	id: root
 
+	readonly property KeyNavigationListHelper __keyNavHelper: keyNavHelper
+
 	width: parent?.width ?? 0
 	height: parent?.height ?? 0
 	boundsBehavior: Flickable.StopAtBounds

components/CommonWords.qml
@@ -19,6 +19,9 @@ QtObject {
 	//% "Add device"
 	readonly property string add_device: qsTrId("common_words_add_device")
 
+	//% "Add Modbus TCP/UDP device"
+	readonly property string add_modbus_device: qsTrId("common_words_add_modbus_device")
+
 	//: The role for an AC input (grid, genset, acload, etc.)
 	//% "Role"
 	readonly property string ac_input_role: qsTrId("common_words_ac_input_role")

components/SettingsColumn.qml
@@ -19,6 +19,7 @@ FocusScope {
 	property alias topPadding: contentColumn.topPadding
 	property alias bottomPadding: contentColumn.bottomPadding
 
+	readonly property KeyNavigationListHelper __keyNavHelper: keyNavHelper
 	default property alias _data: contentColumn.data
 
 	implicitWidth: contentColumn.implicitWidth

components/listitems/core/ListItem.qml
@@ -51,6 +51,10 @@ AbstractListItem {
 
 	// Provide key navigation between list items.
 	KeyNavigationHighlight.active: root.activeFocus
+	KeyNavigationHighlight.topMargin: topInset
+	KeyNavigationHighlight.bottomMargin: bottomInset
+	KeyNavigationHighlight.leftMargin: leftInset
+	KeyNavigationHighlight.rightMargin: rightInset
 	Keys.enabled: Global.keyNavigationEnabled
 
 	background: ListItemBackground {

components/listitems/core/ListQuantityGroupNavigation.qml
@@ -4,6 +4,7 @@
 */
 
 import QtQuick
+import QtQuick.Controls.impl as CP
 import QtQuick.Layouts
 import Victron.VenusOS
 
@@ -16,6 +17,8 @@ ListSetting {
 	property string text
 	property QuantityObjectModel quantityModel
 	property bool tableMode
+	property string iconSource: "qrc:/images/icon_chevron_right_32.svg"
+	property color iconColor: Theme.color_listItem_forwardIcon
 
 	signal clicked
 
@@ -73,13 +76,16 @@ ListSetting {
 			}
 		}
 
-		ForwardIcon {
+		CP.ColorImage {
 			id: arrowIcon
 
 			anchors {
 				right: parent.right
 				verticalCenter: parent.verticalCenter
 			}
+			source: root.iconSource
+			color: root.iconColor
+
 			// Set opacity instead of visible, to maintain vertical alignments across multiple
 			// quantity group list items even when list item is not clickable.
 			opacity: root.interactive ? 1 : 0

images/icon_minus_32.svg
@@ -1,0 +1,4 @@
+<svg width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M5.44365 15C4.89137 15 4.44366 15.4477 4.44365 16C4.44366 16.5523 4.89137 17 5.44365 17C21.1171 17 10.2061 17 26.5563 17C27.1086 17 27.5563 16.5523 27.5563 16C27.5563 15.4477 27.1086 15 26.5563 15C10.2354 15 21.0911 15 5.44365 15Z" fill="white"/>
+</svg>
+

commit 529d5609d2a35df517595a7cca52ea7fac69a6f2

Key navigation: fix navigation in nested views
When a nested view attempts to gives focus to a child, the view must
also be given focus, as it is the FocusScope for the child. Otherwise,
the child will not be given focus in the focus chain.

Regression from 7a73f73e6135978c3105486206e6a923d99eef67.

Fixes #2979

components/KeyNavigationListHelper.qml
@@ -83,7 +83,12 @@ QtObject {
 		}
 		if (containerOrItem.__keyNavHelper) {
 			// Try to focus the first item in the container.
-			return containerOrItem.__keyNavHelper.updateFocusedItem()
+			const result = containerOrItem.__keyNavHelper.updateFocusedItem()
+			if (result) {
+				// Since the container is a FocusScope, we must give it focus as well.
+				containerOrItem.focus = true
+			}
+			return result
 		} else {
 			// Focus the item.
 			containerOrItem.focus = true