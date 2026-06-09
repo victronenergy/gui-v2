c8ddb3071702b370cd94b3c0120f3f3e4434c5ee
Bea Lam <bea.lam@qinetic.com.au>
2026-06-09 11:04:34 +1000

    Switches: hide some input/output settings depending on product ID
    For Aurelia products, some input/output settings should be hidden. We
    will improve this later to configure the setting visibility in the
    backend, instead of hardcoding it in the UI depending on the product id,
    but this is sufficient for now.
    
    Fixes #2941

--- src/productinfo.h
+++ src/productinfo.h
@@ -62,6 +62,7 @@ public:
 		ProductId_PvInverter_Fronius = 0xA142, // VE_PROD_ID_PV_INVERTER_FRONIUS
 		ProductId_TankSensor_Generic = 0xA160,
 		ProductId_MeteoSensor_Imt = 0xB030, // VE_PROD_ID_IMT_SI_RS485_SOLAR_IRRADIANCE_SENSOR
+		ProductId_Dcdb_Aurelia = 0xC0B0,
 	};
 	Q_ENUM(ProductId_Misc)
 
