victronenergy/gui-v2

commit 2746b5bc1dae5964b4b642b9f3a585d03dc7450e

Portrait: adapt tank and environment gauges to portrait layout
In portrait, show the gauges as horizontal instead of vertical bars. To
make this easier, add LevelsGaugeOutline.qml to provide the main layout
for both tank and environment gauges in portrait and landscape.

Add LevelsGaugeOutline, which provides the common label and icon area
around the gauge in both the tank and environment gauge panels. Replace
TankItem with TankGaugePanel, which is similar to EnvironmentGaugePanel.

Also update font sizes, icons and colours to the latest from Figma.

Contributes to #2829

components/EnvironmentGaugePanel.qml
@@ -6,98 +6,189 @@
 import QtQuick
 import Victron.VenusOS
 
-Rectangle {
+AbstractListItem {
 	id: root
 
-	property alias title: header.text
-	property real temperature: NaN
-	property int temperatureType: VenusOS.Temperature_DeviceType_Generic
-	property real humidity: NaN
-	property bool animationEnabled: true
+	required property Device device
+	required property bool animationEnabled
+	required property Gradient temperatureGaugeGradient
+	required property Gradient humidityGaugeGradient
 
-	property var temperatureGaugeGradient
-	property var humidityGaugeGradient
+	readonly property bool hasTwoGauges: !isNaN(temperatureItem.value) && !isNaN(humidityItem.value)
 
-	readonly property int hasTwoGauges: !isNaN(temperature) && !isNaN(humidity)
-	readonly property int _gaugeWidth: hasTwoGauges
-			? (width - (2 * Theme.geometry_levelsPage_panel_border_width)) / 2
-			: Theme.geometry_levelsPage_environment_gauge_width
+	implicitWidth: Math.max(
+		implicitBackgroundWidth + leftInset + rightInset,
+		implicitContentWidth + leftPadding + rightPadding)
+	implicitHeight: Math.max(
+		implicitBackgroundHeight + topInset + bottomInset,
+		implicitContentHeight + topPadding + bottomPadding)
 
-	color: Theme.color_levelsPage_environment_panel_background
-	radius: Theme.geometry_levelsPage_panel_radius
-	border.width: Theme.geometry_levelsPage_panel_border_width
-	border.color: Theme.color_levelsPage_panel_border_color
+	leftInset: Theme.geometry_levelsPage_panel_horizontalInset
+	rightInset: Theme.geometry_levelsPage_panel_horizontalInset
+	leftPadding: leftInset + Theme.geometry_levelsGauge_horizontalPadding
+	rightPadding: rightInset + Theme.geometry_levelsGauge_horizontalPadding
+	topPadding: Theme.geometry_levelsPage_panel_header_height + Theme.geometry_environmentGaugePanel_verticalPadding
+	bottomPadding: Theme.geometry_environmentGaugePanel_verticalPadding
 
-	// Allow key navigation to this item.
-	focus: true
-	focusPolicy: Qt.TabFocus
-	KeyNavigationHighlight.active: root.activeFocus
+	background: Item {
+		implicitWidth: Theme.geometry_levelsPage_panel_background_width
+		implicitHeight: Theme.geometry_levelsPage_panel_background_height
 
-	Behavior on height {
-		enabled: root.animationEnabled
-		NumberAnimation { duration: Theme.animation_page_idleResize_duration; easing.type: Easing.InOutQuad }
-	}
+		// The colour indicator is on the top in landscape, and not visible in portrait.
+		Rectangle {
+			id: colorIndicator
 
-	GaugeHeader {
-		id: header
-		color: Theme.color_levelsPage_panel_border_color
-	}
+			width: parent.width
+			height: Theme.geometry_levelsPage_panel_header_height
+			topLeftRadius: Theme.geometry_levelsPage_panel_radius
+			topRightRadius: Theme.geometry_levelsPage_panel_radius
+			color: Theme.color_environmentGaugePanel_header
+			visible: Theme.screenSize !== Theme.Portrait
 
-	EnvironmentGauge {
-		id: tempGauge
-
-		x: humidityGaugeLoader.active ? 0 : parent.width/2 - width/2
-		anchors {
-			top: header.bottom
-			bottom: parent.bottom
-		}
-		width: root._gaugeWidth
-		animationEnabled: root.animationEnabled
-		icon.source: "qrc:/images/icon_temp_32.svg"
-
-		text: Global.systemSettings.temperatureUnitSuffix
-		value: Math.round(root.temperature)
-		unit: Global.systemSettings.temperatureUnit
-
-		minimumValue: Global.environmentInputs.temperatureGaugeMinimum(root.temperatureType)
-		maximumValue: Global.environmentInputs.temperatureGaugeMaximum(root.temperatureType)
-		stepSize: Global.environmentInputs.temperatureGaugeStepSize(root.temperatureType)
-		highlightedValue: Theme.geometry_levelsPage_environment_temperatureGauge_highlightedValue
-		minimumValueColor: Theme.color_blue
-		maximumValueColor: Theme.color_red
-		highlightedValueColor: Theme.color_levelsPage_environment_temperatureGauge_highlightValue
-		gradient: root.temperatureGaugeGradient
-	}
-
-	Loader {
-		id: humidityGaugeLoader
-
-		anchors {
-			top: header.bottom
-			right: parent.right
-			bottom: parent.bottom
+			Label {
+				anchors {
+					left: parent.left
+					right: parent.right
+					bottom: parent.bottom
+					margins: Theme.geometry_levelsPage_panel_background_title_padding
+				}
+				horizontalAlignment: Text.AlignHCenter
+				elide: Text.ElideRight
+				text: root.device?.name ?? ""
+			}
 		}
 
-		active: !isNaN(root.humidity)
-		sourceComponent: EnvironmentGauge {
-			id: humidityGauge
-
-			width: root._gaugeWidth
-			icon.source: "qrc:/images/icon_humidity_32.svg"
-			// Don't translate. Short local acronyms often don't exist. RH is an international standard.
-			// In case user is not familiar with the acronym "RH" there is also drop icon and percentage sign (%).
-			text: "RH"
-			animationEnabled: root.animationEnabled
-			unit: VenusOS.Units_Percentage
-			value: Math.round(root.humidity)
-			minimumValue: Theme.geometry_levelsPage_environment_humidityGauge_minimumValue
-			maximumValue: Theme.geometry_levelsPage_environment_humidityGauge_maximumValue
-			highlightedValue: Theme.geometry_levelsPage_environment_humidityGauge_highlightedValue
-			minimumValueColor: Theme.color_orange
-			maximumValueColor: Theme.color_blue
-			highlightedValueColor: Theme.color_green
-			gradient: root.humidityGaugeGradient
+		// Fill out the rest of the background with a background colour.
+		Rectangle {
+			anchors {
+				fill: parent
+				topMargin: Theme.geometry_levelsPage_panel_header_height
+			}
+			border.color: Theme.screenSize === Theme.Portrait ? "transparent" : Theme.color_environmentGaugePanel_header
+			color: Theme.color_environmentGaugePanel_background
+			topLeftRadius: colorIndicator.visible ? 0 : Theme.geometry_levelsPage_panel_radius
+			topRightRadius: colorIndicator.visible ? 0 : Theme.geometry_levelsPage_panel_radius
+			bottomLeftRadius: Theme.geometry_levelsPage_panel_radius
+			bottomRightRadius: Theme.geometry_levelsPage_panel_radius
 		}
-		onStatusChanged: if (status === Loader.Error) console.warn("Unable to load himidity environment gauge:", errorString())
+	}
+
+	contentItem: Item {
+		implicitWidth: gaugeFlow.implicitWidth
+		// In landscape, stretch the height to fill the parent.
+		// In portrait, size the height to the gauge content.
+		implicitHeight: Theme.screenSize !== Theme.Portrait ? 0
+				: (nameLabel.text.length ? nameLabel.height : 0) + gaugeFlow.height
+
+		// In portrait, if there are two gauges, show the name here instead of within the outline.
+		Label {
+			id: nameLabel
+
+			anchors {
+				left: parent.left
+				leftMargin: Theme.geometry_icon_size_medium + Theme.geometry_levelsGauge_horizontalSpacing
+				right: parent.right
+			}
+			font.pixelSize: Theme.font_levelsGauge_title
+			wrapMode: Text.Wrap
+			text: Theme.screenSize === Theme.Portrait && root.hasTwoGauges ? root.device?.name ?? "" : ""
+			color: Theme.color_font_primary
+		}
+
+		Flow {
+			id: gaugeFlow
+
+			readonly property int gaugeWidth: Theme.screenSize === Theme.Portrait ? parent.width
+					: hasTwoGauges ? (parent.width - (2 * Theme.geometry_levelsPage_panel_border_width)) / 2
+					: Theme.geometry_environmentGauge_width
+
+			anchors {
+				horizontalCenter: parent.horizontalCenter
+				top: nameLabel.text.length ? nameLabel.bottom : undefined
+			}
+			width: Theme.screenSize === Theme.Portrait ? parent.width
+					: root.hasTwoGauges ? gaugeWidth * 2
+					: gaugeWidth
+			flow: Theme.screenSize === Theme.Portrait ? Flow.TopToBottom : Flow.LeftToRight
+			spacing: Theme.screenSize === Theme.Portrait ? Theme.geometry_levelsGauge_verticalPadding : 0
+
+			LevelsGaugeOutline {
+				width: gaugeFlow.gaugeWidth
+				height: Theme.screenSize === Theme.Portrait ? implicitHeight : gaugeFlow.parent.height
+				name: root.hasTwoGauges ? "" : (root.device?.name ?? "")
+				iconSource: "qrc:/images/icon_temp_32.svg"
+				value: temperatureItem.valid ? Math.round(temperatureItem.value) : NaN
+				unit: Global.systemSettings.temperatureUnit
+				unitText: Units.defaultUnitString(Global.systemSettings.temperatureUnit)
+				quantityFormatHints: Theme.screenSize === Theme.Portrait ? 0 : Units.CompactUnitFormat
+				gaugeHorizontalPadding: Theme.geometry_environmentGauge_gauge_horizontalPadding
+
+				gauge: EnvironmentGauge {
+					orientation: Theme.screenSize === Theme.Portrait ? Qt.Horizontal : Qt.Vertical
+					value: temperatureItem.value ?? NaN
+					minimumValue: Global.environmentInputs.temperatureGaugeMinimum(root.temperatureType)
+					maximumValue: Global.environmentInputs.temperatureGaugeMaximum(root.temperatureType)
+					stepSize: Global.environmentInputs.temperatureGaugeStepSize(root.temperatureType)
+					highlightedValue: Theme.geometry_levelsPage_environment_temperatureGauge_highlightedValue
+					minimumValueColor: Theme.color_blue
+					maximumValueColor: Theme.color_red
+					highlightedValueColor: Theme.color_levelsPage_environment_temperatureGauge_highlightValue
+					gradient: root.temperatureGaugeGradient
+					animationEnabled: root.animationEnabled
+				}
+			}
+
+			LevelsGaugeOutline {
+				width: gaugeFlow.gaugeWidth
+				height: Theme.screenSize === Theme.Portrait ? implicitHeight : gaugeFlow.parent.height
+				visible: !isNaN(humidityItem.value)
+				name: root.hasTwoGauges ? "" : (root.device?.name ?? "")
+				iconSource: "qrc:/images/icon_humidity_32.svg"
+				value: humidityItem.valid ? Math.round(humidityItem.value) : NaN
+				unit: VenusOS.Units_Percentage
+				// Don't translate. Short local acronyms often don't exist. RH is an international standard.
+				// In case user is not familiar with the acronym "RH" there is also drop icon and percentage sign (%).
+				unitText: "RH"
+				quantityFormatHints: Theme.screenSize === Theme.Portrait ? 0 : Units.CompactUnitFormat
+				gaugeHorizontalPadding: Theme.geometry_environmentGauge_gauge_horizontalPadding
+				gauge: isNaN(humidityItem.value) ? null : humidityGaugeComponent
+
+				Component {
+					id: humidityGaugeComponent
+
+					EnvironmentGauge {
+						orientation: Theme.screenSize === Theme.Portrait ? Qt.Horizontal : Qt.Vertical
+						value: humidityItem.value ?? NaN
+						minimumValue: Theme.geometry_levelsPage_environment_humidityGauge_minimumValue
+						maximumValue: Theme.geometry_levelsPage_environment_humidityGauge_maximumValue
+						stepSize: Theme.geometry_environmentGauge_tick_step
+						highlightedValue: Theme.geometry_levelsPage_environment_humidityGauge_highlightedValue
+						minimumValueColor: Theme.color_orange
+						maximumValueColor: Theme.color_blue
+						highlightedValueColor: Theme.color_green
+						gradient: root.humidityGaugeGradient
+						animationEnabled: root.animationEnabled
+					}
+				}
+			}
+		}
+
+		VeQuickItem {
+			id: temperatureType
+			readonly property int intValue: valid ? value : VenusOS.Temperature_DeviceType_Generic
+			uid: root.device ? root.device.serviceUid + "/TemperatureType" : ""
+		}
+	}
+
+	VeQuickItem {
+		id: humidityItem
+		uid: root.device ? root.device.serviceUid + "/Humidity" : ""
+	}
+
+	VeQuickItem {
+		id: temperatureItem
+		uid: root.device ? root.device.serviceUid + "/Temperature" : ""
+		sourceUnit: Units.unitToVeUnit(VenusOS.Units_Temperature_Celsius)
+		displayUnit: Units.unitToVeUnit(Global.systemSettings.temperatureUnit)
 	}
 }

cmake/ModuleVenus_Sources.cmake
@@ -56,7 +56,6 @@ set (VictronVenusOS_QML_MODULE_SOURCES
     components/FixedWidthLabel.qml
     components/ForwardIcon.qml
     components/GaugeModel.qml
-    components/GaugeHeader.qml
     components/GeneratorManualControlButton.qml
     components/GensetStartStop1Finder.qml
     components/FlatListItemSeparator.qml
@@ -70,6 +69,7 @@ set (VictronVenusOS_QML_MODULE_SOURCES
     components/InverterAcOutSettings.qml
     components/IOChannelQuantityLabel.qml
     components/Led.qml
+    components/LevelsGaugeOutline.qml
     components/LoadGraph.qml
     components/LoadGraphShapePath.qml
     components/KeyNavigationListHelper.qml
@@ -121,8 +121,8 @@ set (VictronVenusOS_QML_MODULE_SOURCES
     components/SystemBatteryDeviceModel.qml
     components/SystemReason.qml
     components/TabBar.qml
-    components/TankItem.qml
     components/TankGauge.qml
+    components/TankGaugePanel.qml
     components/TemperatureRelaySettings.qml
     components/ThreeLabelLayout.qml
     components/ThreePhaseBarGauge.qml
@@ -787,23 +787,23 @@ set(VictronVenusOS_RESOURCES
     images/freshWater.svg
     images/key_navigation_highlight_dark.svg
     images/key_navigation_highlight_light.svg
-    images/icon_black_water_24.svg
+    images/icon_black_water_32.svg
     images/icon_charging_generator.svg
     images/icon_charging_grid.svg
     images/icon_charging_renewables.svg
     images/icon_charging_shore.svg
     images/icon_CL_24.svg
     images/icon_trash_32.svg
-    images/icon_fresh_water_24.svg
-    images/icon_raw_water_24.svg
-    images/icon_waste_water_24.svg
-    images/icon_livewell_24.svg
-    images/icon_fuel_24.svg
+    images/icon_fresh_water_32.svg
+    images/icon_raw_water_32.svg
+    images/icon_waste_water_32.svg
+    images/icon_livewell_32.svg
+    images/icon_fuel_32.svg
     images/icon_from_grid.svg
-    images/icon_oil_24.svg
-    images/icon_hydraulic_oil_24.svg
-    images/icon_lng_24.svg
-    images/icon_lpg_24.svg
+    images/icon_oil_32.svg
+    images/icon_hydraulic_oil_32.svg
+    images/icon_lng_32.svg
+    images/icon_lpg_32.svg
     images/icon_open_link_32.svg
     images/icon_to_grid.svg
     images/fueltank.svg

components/BarGaugeBase.qml
@@ -19,9 +19,9 @@ Rectangle {
 	property int orientation: Qt.Vertical
 	property bool animationEnabled
 
-	color: backgroundColor
 	width: orientation === Qt.Vertical ? Theme.geometry_barGauge_vertical_width_large : parent.width
 	height: orientation === Qt.Vertical ? parent.height : Theme.geometry_barGauge_horizontal_height
+	color: backgroundColor
 	radius: orientation === Qt.Vertical ? width / 2 : height / 2
 
 	readonly property real _value: isNaN(value) ? 0 : Math.min(1.0, Math.max(0, value))

components/CheapBarGauge.qml
@@ -33,8 +33,6 @@ Rectangle {
 	property bool animationEnabled
 
 	color: backgroundColor
-	width: _isVertical ? Theme.geometry_barGauge_vertical_width_large : parent.width
-	height: _isVertical ? parent.height : Theme.geometry_barGauge_horizontal_height
 	radius: _isVertical ? width / 2 : height / 2
 
 	readonly property bool _isVertical: orientation === Qt.Vertical

components/EnvironmentGauge.qml
@@ -5,189 +5,166 @@
 
 import QtQuick
 import Victron.VenusOS
-import QtQuick.Controls.impl as CP
 
 Item {
 	id: root
 
-	property alias icon: typeIcon
-	property alias text: typeLabel.text
-	property alias value: quantityLabel.value
-	property alias unit: quantityLabel.unit
+	required property int minimumValue
+	required property int maximumValue
+	required property int stepSize
+	required property int highlightedValue
+	required property color minimumValueColor
+	required property color maximumValueColor
+	required property color highlightedValueColor
+	required property Gradient gradient
+	required property bool animationEnabled
+	required property real value
+	property int orientation: Qt.Vertical
 
-	property int minimumValue: 0
-	property int maximumValue: 100
-	property int stepSize: Theme.geometry_levelsPage_environment_gauge_tick_step
-	property int highlightedValue: 0
-	property color minimumValueColor
-	property color maximumValueColor
-	property color highlightedValueColor
-
-	property alias gradient: gaugeBar.gradient
-	property bool animationEnabled
-
-	function _barYPosForValue(v) {
+	function _barPosForValue(v) {
 		const range = maximumValue - minimumValue
 		if (range === 0) {
 			console.warn("Ignoring zero range! Min:", minimumValue, "Max:", maximumValue)
 			return 0
 		}
-		const tickHeight = Theme.geometry_levelsPage_environment_gauge_tick_size
-		const unitsPerPixel = (gaugeTicks.height - tickHeight) / range
+		const tickSize = Theme.geometry_environmentGauge_tick_size
+		const totalLength = orientation === Qt.Horizontal ? gaugeBar.width : gaugeTicks.height
+		const unitsPerPixel = (totalLength - tickSize) / range
 		const unitCount = v - minimumValue
-		const pos = gaugeBar.height - (gaugeTicks.anchors.bottomMargin + (unitsPerPixel * unitCount)) - tickHeight/2
-		return Math.max(0, Math.min(pos, gaugeBar.height))
-	}
-
-	width: Theme.geometry_levelsPage_environment_gauge_width
-	height: parent.height
-
-	CP.ColorImage {
-		id: typeIcon
-
-		anchors {
-			top: parent.top
-			topMargin: Theme.geometry_levelsPage_environment_gauge_icon_topMargin
-			horizontalCenter: parent.horizontalCenter
+		let pos = (unitsPerPixel * unitCount) + tickSize/2
+		if (orientation === Qt.Vertical) {
+			// For vertical bars, the value is inverted, i.e. the min value is at the bottom.
+			pos = totalLength - pos
 		}
-		width: Theme.geometry_levelsPage_environment_gauge_icon_size
-		height: Theme.geometry_levelsPage_environment_gauge_icon_size
-		fillMode: Image.Pad
-		color: Theme.color_font_primary
+		return Math.max(0, Math.min(pos, totalLength))
 	}
 
-	Label {
-		id: typeLabel
+	implicitHeight: root.orientation === Qt.Vertical ? 0
+			: gaugeBar.height + gaugeTicks.height + Theme.geometry_levelsGauge_verticalPadding
 
-		anchors {
-			top: typeIcon.bottom
-			topMargin: Theme.geometry_levelsPage_environment_gauge_typeLabel_topMargin
-			horizontalCenter: parent.horizontalCenter
-		}
-		font.pixelSize: Theme.font_size_body2
-		color: Theme.color_font_primary
+	Rectangle {
+		id: gaugeBar
+
+		x: root.orientation === Qt.Horizontal ? 0
+			: parent.width/2 - valueMarker.width + Theme.geometry_environmentGauge_tick_size/2
+		implicitWidth: root.orientation === Qt.Vertical ? Theme.geometry_barGauge_vertical_width_large : parent.width
+		implicitHeight: root.orientation === Qt.Vertical
+				? parent.height - bottomSeparator.height - 2*Theme.geometry_levelsGauge_verticalPadding
+				: Theme.geometry_barGauge_horizontal_height
+		radius: root.orientation === Qt.Vertical ? width / 2 : height / 2
+		gradient: root.gradient
+		rotation: root.orientation === Qt.Vertical ? 0 : 180 // when horizontal, min is on left, max on right
 	}
 
-	Column {
+	Flow {
 		id: gaugeTicks
 
 		anchors {
-			top: gaugeBar.top
-			bottom: gaugeBar.bottom
-			topMargin: Theme.geometry_levelsPage_environment_gauge_bar_padding
-			bottomMargin: Theme.geometry_levelsPage_environment_gauge_bar_padding
-			horizontalCenter: parent.horizontalCenter
+			horizontalCenter: root.orientation === Qt.Vertical ? parent.horizontalCenter : gaugeBar.horizontalCenter
+			verticalCenter: root.orientation === Qt.Vertical ? gaugeBar.verticalCenter : undefined
+			top: root.orientation === Qt.Vertical ? undefined : gaugeBar.bottom
+			topMargin: Theme.geometry_levelsGauge_verticalPadding
 		}
-		spacing: (height - (tickRepeater.count * Theme.geometry_levelsPage_environment_gauge_tick_size)) / (tickRepeater.count - 1)
+		flow: root.orientation === Qt.Vertical ? Flow.TopToBottom : Flow.LeftToRight
+		spacing: ((root.orientation === Qt.Vertical ? gaugeBar.height - 2*Theme.geometry_environmentGauge_verticalPadding : gaugeBar.width)
+					- (tickRepeater.count * Theme.geometry_environmentGauge_tick_size))
+						/ (tickRepeater.count - 1)
 
 		Repeater {
 			id: tickRepeater
 
 			model: (root.maximumValue + root.stepSize - root.minimumValue) / root.stepSize
 			delegate: Item {
+				required property int index
 				readonly property int tickValue: {
-					const invertedIndex = tickRepeater.count - index - 1
-					return minimumValue + (root.stepSize * invertedIndex)
+					// For vertical bars, the value is inverted, i.e. the min value is at the bottom.
+					const tickIndex = root.orientation === Qt.Vertical ? tickRepeater.count - index - 1 : index
+					return minimumValue + (root.stepSize * tickIndex)
 				}
 
-				width: Theme.geometry_levelsPage_environment_gauge_tick_size
-				height: Theme.geometry_levelsPage_environment_gauge_tick_size
+				width: Theme.geometry_environmentGauge_tick_size
+				height: Theme.geometry_environmentGauge_tick_size
 
 				Label {
-					id: tickLabel
-
 					anchors.verticalCenter: parent.verticalCenter
-					leftPadding: Theme.geometry_levelsPage_environment_gauge_tick_margin
-					text: model.index === 0 ? root.maximumValue
-						: model.index === tickRepeater.count - 1 ? root.minimumValue
+					anchors.horizontalCenter: root.orientation === Qt.Vertical ? undefined : parent.horizontalCenter
+					leftPadding: Theme.geometry_environmentGauge_tick_margin
+					text: tickValue === root.maximumValue ? root.maximumValue
+						: tickValue === root.minimumValue ? root.minimumValue
 						: tickValue === root.highlightedValue ? tickValue
 						: ""
-					color: Theme.color_levelsPage_environment_gauge_tickText
-					font.pixelSize: Theme.font_size_caption
+					color: Theme.color_font_secondary
+					font.pixelSize: Theme.font_levelsGauge_secondary
 				}
 
 				Rectangle {
 					anchors.verticalCenter: parent.verticalCenter
-					width: Theme.geometry_levelsPage_environment_gauge_tick_size
-					height: Theme.geometry_levelsPage_environment_gauge_tick_size
-					radius: Theme.geometry_levelsPage_environment_gauge_tick_size
-					color: model.index === 0 ? root.maximumValueColor
-						 : model.index === tickRepeater.count - 1 ? root.minimumValueColor
+					width: Theme.geometry_environmentGauge_tick_size
+					height: Theme.geometry_environmentGauge_tick_size
+					radius: Theme.geometry_environmentGauge_tick_size
+					color: tickValue === root.maximumValue ? root.maximumValueColor
+						 : tickValue === root.minimumValue ? root.minimumValueColor
 						 : tickValue === root.highlightedValue ? root.highlightedValueColor
 						 : Theme.color_levelsPage_environment_gauge_tick
+					visible: root.orientation === Qt.Vertical
 				}
 			}
 		}
 	}
 
 	Rectangle {
-		id: gaugeBar
+		id: valueMarker
 
-		anchors {
-			top: typeLabel.bottom
-			topMargin: Theme.geometry_levelsPage_environment_gauge_bar_topMargin
-			bottom: quantitySeparator.top
-			bottomMargin: Theme.geometry_levelsPage_environment_gauge_bar_bottomMargin
-			right: gaugeTicks.left
-			rightMargin: Theme.geometry_levelsPage_environment_gauge_tick_margin
+		visible: !isNaN(root.value)
+		x: root.orientation === Qt.Vertical ? gaugeBar.x : root._barPosForValue(root.value) - width/2
+		y: root.orientation === Qt.Vertical ? Theme.geometry_environmentGauge_verticalPadding + root._barPosForValue(root.value) - height/2 : 0
+		width: root.orientation === Qt.Vertical
+			   ? Theme.geometry_environmentGauge_valueMarker_background_width
+			   : gaugeBar.height
+		height: root.orientation === Qt.Vertical
+				? Theme.geometry_environmentGauge_valueMarker_background_height
+				: gaugeBar.height
+		color: Theme.color_environmentGaugePanel_background
+		radius: root.orientation === Qt.Horizontal ? height / 2 : 0
+
+		Behavior on x {
+			// Only animate when the value has changed, and not when the page is resizing
+			enabled: root.animationEnabled && (!Global.pageManager?.animatingIdleResize)
+					&& root.orientation === Qt.Horizontal
+			XAnimator {}
 		}
-		width: Theme.geometry_levelsPage_environment_gauge_bar_width
-		radius: width/2
+		Behavior on y {
+			enabled: root.animationEnabled && (!Global.pageManager?.animatingIdleResize)
+					 && root.orientation === Qt.Vertical
+			YAnimator {}
+		}
 
 		Rectangle {
-			id: valueMarker
-
-			visible: !isNaN(root.value)
-			y: root._barYPosForValue(root.value) - height/2
-			width: parent.width
-			height: Theme.geometry_levelsPage_environment_gauge_valueMarker_background_height
-			color: Theme.color_levelsPage_environment_panel_background
-
-			Behavior on y {
-				// Only animate when the value has changed, and not when the page is resizing
-				enabled: root.animationEnabled && (!!Global.pageManager && !Global.pageManager.animatingIdleResize)
-				YAnimator {}
+			anchors {
+				fill: parent
+				leftMargin: root.orientation === Qt.Vertical ? 0 : Theme.geometry_environmentGauge_valueMarker_padding
+				rightMargin: root.orientation === Qt.Vertical ? 0 : Theme.geometry_environmentGauge_valueMarker_padding
+				topMargin: Theme.geometry_environmentGauge_valueMarker_padding
+				bottomMargin: Theme.geometry_environmentGauge_valueMarker_padding
 			}
+			color: Theme.color_font_primary
 
-			Rectangle {
-				anchors.verticalCenter: parent.verticalCenter
-				width: gaugeBar.width + Theme.geometry_levelsPage_environment_gauge_tick_margin
-					   + Theme.geometry_levelsPage_environment_gauge_tick_size
-				radius: height/2
-				height: Theme.geometry_levelsPage_environment_gauge_tick_size
-				color: Theme.color_font_primary
-
-				// Asymmetrical rounding: don't round rectangle on top-left and bottom-left corners
-				Rectangle {
-					height: parent.height
-					width: parent.radius
-					color: parent.color
-				}
-			}
+			// When horizontal, round all corners. In vertical, only round right-edge corners.
+			topLeftRadius: root.orientation === Qt.Horizontal ? height / 2 : 0
+			bottomLeftRadius: root.orientation === Qt.Horizontal ? height / 2 : 0
+			topRightRadius: height / 2
+			bottomRightRadius: height / 2
 		}
 	}
 
 	SeparatorBar {
-		id: quantitySeparator
+		id: bottomSeparator
 
 		anchors {
 			left: parent.left
-			leftMargin: Theme.geometry_levelsPage_environment_gauge_separator_horizontalMargin
 			right: parent.right
-			rightMargin: Theme.geometry_levelsPage_environment_gauge_separator_horizontalMargin
-			bottom: quantityLabel.top
-		}
-	}
-
-	QuantityLabel {
-		id: quantityLabel
-
-		anchors {
 			bottom: parent.bottom
-			horizontalCenter: parent.horizontalCenter
 		}
-		font.pixelSize: Theme.font_size_h1
-		height: Theme.geometry_levelsPage_environment_gauge_quantityLabel_height
-		formatHints: Units.CompactUnitFormat
+		visible: root.orientation === Qt.Vertical
 	}
 }

components/ExpandedTanksView.qml
@@ -10,32 +10,24 @@ import Victron.Gauges
 BaseListView {
 	id: root
 
-	property bool animationEnabled
+	required property bool animationEnabled
 
-	orientation: Qt.Horizontal
-	spacing: Gauges.spacing(count)
-	delegate: TankItem {
+	orientation: Theme.screenSize === Theme.Portrait ? ListView.Vertical : ListView.Horizontal
+	spacing: Theme.screenSize === Theme.Portrait ? Theme.geometry_levelsPage_gauge_spacing_tiny : Gauges.spacing(count)
+	delegate: TankGaugePanel {
 		id: tankDelegate
 
 		required property Tank device
 
-		width: Gauges.width(root.model.count, Theme.geometry_levelsPage_max_tank_count, Theme.geometry_screen_width)
-		height: Theme.geometry_levelsPage_panel_expanded_height
-		status: device.status
-		fluidType: device.type
-		name: device.name
-		level: device.level
-		totalCapacity: device.capacity
-		totalRemaining: device.remaining
-
-		gauge: TankGauge {
-			width: Theme.geometry_levelsPage_groupedSubgauges_delegate_width
-			valueType: tankDelegate.tankProperties.valueType
-			animationEnabled: root.animationEnabled
-			value: tankDelegate.device.level / 100
-			surfaceColor: tankDelegate.status === VenusOS.Tank_Status_Ok
-				? Theme.color_levelsPage_gauge_separatorBarColor
-				: tankDelegate.backgroundColor
-		}
+		width: root.orientation === ListView.Vertical
+			   ? ListView.view.width
+			   : Gauges.width(root.model.count, Theme.geometry_levelsPage_max_tank_count, Theme.geometry_screen_width)
+		height: root.orientation === ListView.Vertical
+			   ? implicitHeight
+			   : ListView.view.height
+		animationEnabled: root.animationEnabled
+		tank: device
+		tankModel: null
+		isGroup: false
 	}
 }

commit a2721f9ce8e1d1b12993067c710e1815c2ca309f

Levels: fix min/max values for temperature gauges
Avoid referencing `root.temperatureType`, which is no longer defined.

Regression from 2746b5bc1dae5964b4b642b9f3a585d03dc7450e.

Fixes #3143

components/EnvironmentGaugePanel.qml
@@ -126,9 +126,9 @@ AbstractListItem {
 				gauge: EnvironmentGauge {
 					orientation: Theme.screenSize === Theme.Portrait ? Qt.Horizontal : Qt.Vertical
 					value: temperatureItem.value ?? NaN
-					minimumValue: Global.environmentInputs.temperatureGaugeMinimum(root.temperatureType)
-					maximumValue: Global.environmentInputs.temperatureGaugeMaximum(root.temperatureType)
-					stepSize: Global.environmentInputs.temperatureGaugeStepSize(root.temperatureType)
+					minimumValue: Global.environmentInputs.temperatureGaugeMinimum(temperatureType.value)
+					maximumValue: Global.environmentInputs.temperatureGaugeMaximum(temperatureType.value)
+					stepSize: Global.environmentInputs.temperatureGaugeStepSize(temperatureType.value)
 					highlightedValue: Theme.geometry_levelsPage_environment_temperatureGauge_highlightedValue
 					minimumValueColor: Theme.color_blue
 					maximumValueColor: Theme.color_red