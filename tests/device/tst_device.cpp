f7aebee36a8b54d04bcc19259f07f1ed4df21bff
Chris Adams <chris.adams@qinetic.com.au>
2026-08-27 14:11:44 +1000

    Dev: fix installation on desktop platforms
    Previously, installation was only enabled for VENUS_GX_BUILD
    but not for other platforms.  This commit adds the appropriate
    build stuff to ensure that installs work on all platforms.
    
    e.g. now the following works:
    
    ```
    cd C:\work\Victron\gui-v2-build
    C:\Qt\6.8.3\msvc2022_64\bin\qt-cmake -DCMAKE_BUILD_TYPE=MinSizeRel C:\work\Victron\gui-v2
    cmake --build . --config MinSizeRel --parallel 12
    cmake --install . --config MinSizeRel --prefix C:\work\Victron\gui-v2-install
    ```
    
    On windows, it runs windeployqt and qmlimportscanner etc.
    It does NOT install the MSVC redistributable as part of this,
    as it assumes that gui-v2 is always built/run on developer machines.
    
    Contributes to issue #3181

--- tests/device/tst_device.cpp
+++ tests/device/tst_device.cpp
@@ -3,12 +3,8 @@
 ** See LICENSE.txt for license information.
 */
 
-#include <QtQuickTest/quicktest.h>
 #include "backendconnection.h"
 
-int main(int argc, char **argv)
-{
-    QTEST_SET_MAIN_SOURCE_PATH
-    Victron::VenusOS::BackendConnection::create()->setType(Victron::VenusOS::BackendConnection::MockSource);
-    return quick_test_main(argc, argv, "tst_device", "../tests/device/");
-}
+#include "testutils.h"
+
+VENUS_MOCK_TEST_MAIN(device)
--- tests/notificationtoast/tst_notificationtoast.cpp
+++ tests/notificationtoast/tst_notificationtoast.cpp
@@ -7,18 +7,21 @@
 #include "backendconnection.h"
 #include "veqitemmockproducer.h"
 
-using namespace Victron::VenusOS;
+#include "testutils.h"
 
 int main(int argc, char **argv)
 {
 	QTEST_SET_MAIN_SOURCE_PATH
-	BackendConnection::create()->setType(BackendConnection::MockSource);
+	Victron::VenusOS::BackendConnection::create()->setType(
+			Victron::VenusOS::BackendConnection::MockSource);
 
 	// Create the platform Notifications parent item (as venus-platform does at startup).
 	// This ensures NotificationModel.init() succeeds on its first attempt.
 	// Slot children are created dynamically by individual tests.
-	auto *producer = qobject_cast<VeQItemMockProducer *>(BackendConnection::create()->producer());
+	auto *producer = qobject_cast<Victron::VenusOS::VeQItemMockProducer *>(
+			Victron::VenusOS::BackendConnection::create()->producer());
 	producer->setValue("com.victronenergy.platform/Notifications/NumberOfNotifications", 0);
 
-	return quick_test_main(argc, argv, "tst_notificationtoast", "../tests/notificationtoast/");
+	const QByteArray srcDir = resolveTestSourceDir("notificationtoast", argv[0]).toLocal8Bit();
+	return quick_test_main(argc, argv, "tst_notificationtoast", srcDir.constData());
 }
